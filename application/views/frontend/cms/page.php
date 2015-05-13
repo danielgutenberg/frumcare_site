783aa96513215e9721114ca1dfb01913ae851ec1
danielguten <danielguten@gmail.com>
2015-05-13 15:09:25 +0000

    fix class in cms pages

--- application/views/frontend/cms/page.php
+++ application/views/frontend/cms/page.php
@@ -22,7 +22,7 @@
 		</ul>
 	</div>
     <?php if($content_data){?> 
-        <div class="right-sidebar-resource">
+        <div class="right-side-resources">
          	<p> <?php echo nl2br($content_data['content']); 
          
     }?></p></div>
