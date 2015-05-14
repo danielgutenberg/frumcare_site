0a19dfdc7201972adc202d3297f63e43401ba49b
danielguten <danielguten@gmail.com>
2015-05-14 06:28:35 +0000

    fix archiving confirm messages

--- application/views/frontend/user/profile.php
+++ application/views/frontend/user/profile.php
@@ -92,12 +92,12 @@ if(check_user()) {
                     </td>
                     <?php if ($row->profile_status == 2) { ?>
                         <td width="15%">
-                            <a href="<?php echo site_url();?>user/unarchive_profile/<?php echo $this->session->userdata['current_user'];?>/<?php echo $row->care_type;?>" onclick="return confirm('Are you sure you want to archive this profile?')">
+                            <a href="<?php echo site_url();?>user/unarchive_profile/<?php echo $this->session->userdata['current_user'];?>/<?php echo $row->care_type;?>" onclick="return confirm('Are you sure you want to activate this profile?')">
                             Unarchive &nbsp;
                         </td>
                     <?php } else { ?>
                     <td width="15%">
-                        <a href="<?php echo site_url();?>user/delete_profile/<?php echo $this->session->userdata['current_user'];?>/<?php echo $row->care_type;?>" onclick="return confirm('Are you sure you want to repost this profile?')">
+                        <a href="<?php echo site_url();?>user/delete_profile/<?php echo $this->session->userdata['current_user'];?>/<?php echo $row->care_type;?>" onclick="return confirm('Are you sure you want to archive this profile?')">
                         Archive &nbsp;
                     </td>
                     <?php } ?>
