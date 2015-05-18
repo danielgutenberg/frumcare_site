cde114be90e5e4993763ac70beb15aebf9fe4095
pramod <pramod@ranjana>
2015-05-18 20:53:34 +0545

    organization type addition in senior care center

--- application/views/frontend/care/giver/edit_seniorcarecenter_form.php
+++ application/views/frontend/care/giver/edit_seniorcarecenter_form.php
@@ -35,7 +35,7 @@
             </div>
             
             <?php
-                print_r(detail); 
+                print_r($detail); 
             ?>
             <div>
                 <label>Type of Organization</label>
