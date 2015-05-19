bfc242ea0c7c74d5dfd42ffa0406ed4405107a56
danielgutenberg <daniel@tarcha.com>
2015-05-19 08:54:48 +0000

    fix default values in sign up form

--- application/views/frontend/signup/signup_form.php
+++ application/views/frontend/signup/signup_form.php
@@ -124,8 +124,10 @@ if($this->uri->segment(2)!='edit'){
 <script type="text/javascript">
     function initialize() {
         var acc_category = $('input[name=account_category]:checked').val();
-        console.log(acc_category)
-        getAccountCat(2,1)
+        if (acc_category == 2) {
+            getAccountCat(2,1)
+        }
+        
     }
     
     $(function(){
