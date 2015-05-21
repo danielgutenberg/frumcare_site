089f29826aad0952d72cd3ec0431792e61331831
pramod <pramod@ranjana>
2015-05-21 17:17:18 +0545

    sms test

---
+++ application/views/frontend/smstest.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<form action="<?php site_url('welcome/smstest'); ?>">
+    <input type="text" name="sms" value="">
+    <input type="submit" name="submit" value="Send SMS">
+</form>
