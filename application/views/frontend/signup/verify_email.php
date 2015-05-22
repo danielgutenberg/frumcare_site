5d51765fcee8d378e17cfebd5a4014d8df30dff3
danielguten <danielguten@gmail.com>
2015-05-22 07:02:27 +0000

    change signup success page

--- application/views/frontend/signup/verify_email.php
+++ application/views/frontend/signup/verify_email.php
@@ -2,7 +2,7 @@
     <div class="sign_up_successful">
     <h2>Welcome to the Frumcare Family!</h2> 
     <p>Your account has been successfully created. We need to verify your email. Please press on the link in the email we sent you.</p>
-    <p>You entered <?php echo $user[0]['email'] ?></p>
+    <p>You entered with <?php echo $user[0]['email'] ?></p>
     <p><a href="">Click Here</a> to resend the email.</p>
     <p><?php             
             if(is_array($redirectData)){
@@ -15,7 +15,7 @@
                 $link = site_url().'user/profile';
         ?></p>
     <p><a href="<?php echo $link;?>">Click Here</a> to post an ad.</p>
-    <p><a href="<?php echo base_url('user/edit/'.sha1(check_user())) ?>">Click Here</a> to update your email.</p>
+    <p><a href="<?php echo base_url('user/edit/'.sha1(check_user())) ?>">Click Here</a> to update your email address.</p>
     <p>Thanks!</p>
     </div>
 </div>
