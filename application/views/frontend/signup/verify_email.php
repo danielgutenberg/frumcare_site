17c72eb1a288cbdfc3b5fe80fb58ad938c9fb5a0
danielguten <danielguten@gmail.com>
2015-05-22 06:50:16 +0000

    debug

--- application/views/frontend/signup/verify_email.php
+++ application/views/frontend/signup/verify_email.php
@@ -3,7 +3,8 @@
     <div class="sign_up_successful">
     <h2>Welcome to the Frumcare Family!</h2> 
     <p>Your account has been successfully created. We need to verify your email. Please press on the link in the email we sent you.</p>
-    <p>You entered</p>
+    <p>You entered <?php echo check_user() ?></p>
+    <p><a href="<?php echo base_url('user/edit/'.sha1(check_user())) ?>">Click Here</a> to update your email.</p>
     <p><a href="">Click Here</a> to resend the email.</p>
     <p><?php             
             if(is_array($redirectData)){
