28905cba29d8cc956b1374ce847f7b7e28131d15
danielguten <danielguten@gmail.com>
2015-05-25 12:08:03 +0000

    give admin access to user dashboard

--- application/controllers/admin/user.php
+++ application/controllers/admin/user.php
@@ -22,19 +22,13 @@ class User extends CI_Controller
     }
     
     function dashboard($id) {
-        // $ac_cat = $this->user_model->get_my_account_category($id);
-        // print_r($ac_cat);
-        $sess = array(
-            'current_user' => $id
-            // 'log_id' => $log_id
-            // 'account_category' => $ac_cat[0]['account_category'],
-            // 'organization_care' => $ac_cat[0]['organization_care']
-        );
+        $this->breadcrumbs->push('Dashboard', site_url().'#');
+        $this->breadcrumbs->unshift('My Account', base_url().'user/dashboard');
 
-        $this->session->sess_expiration = '14400';
-        $this->session->set_userdata($sess);
-        echo 'got here';
-        redirect('user/dashboard', 'refresh');
+        $data['main_content']       = 'frontend/user/dashboard';
+        $data['verificationdata']   = $this->user_model->getVerificationData($id);
+        $data['title']              = 'Dashboard';
+        $this->load->view(FRONTEND_TEMPLATE, $data);
     }
     
     function logs(){       
