81dc5f33a2639b896e73b56e896530ec91bd9825
danielguten <danielguten@gmail.com>
2015-05-31 16:46:12 +0000

    debug

--- application/libraries/clickatell.php
+++ application/libraries/clickatell.php
@@ -73,7 +73,7 @@ class Clickatell {
     public function send($number,$text){
             $sendtext=urlencode($text);
             $phone=urlencode($number);
-            $phone = '972' . ltrim ($phone, '0');
+            $phone = '001' . ltrim ($phone, '0');
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,$this->send_url);
             curl_setopt($ch, CURLOPT_POST, 3);
