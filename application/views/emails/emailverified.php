26b01773ad066004c6c0b06b0a15314a22d0a6aa
danielguten <danielguten@gmail.com>
2015-06-04 20:40:12 +0000

    fix wording of email verified email

--- application/views/emails/emailverified.php
+++ application/views/emails/emailverified.php
@@ -4,7 +4,7 @@
         <p style="font-family:Arial, Helvetica, sans-serif; font-size:13px; margin-bottom:20px;">Your email has been successfully verified.<br>Please use the following details to login your FrumCare account.<p>
         	<div style="font-family:Arial, Helvetica, sans-serif; font-size:13px; margin-bottom:5px;">User email: <?php echo $email;?></div>
         	<div style="font-family:Arial, Helvetica, sans-serif; font-size:13px; margin-bottom:5px;">Password: <?php echo $password;?></div>
-        	<div style="font-family:Arial, Helvetica, sans-serif; font-size:13px; margin-bottom:5px;">Link: <a href="<?php echo site_url();?>login">Click Here</a> to manage your account.</div>
+        	<div style="font-family:Arial, Helvetica, sans-serif; font-size:13px; margin-bottom:5px;"><a href="<?php echo site_url();?>login">Click Here</a> to manage your account.</div>
     </div>
 <br />
 Thank you,
