774e8af756fdcbc741e9f02a7ba20533200502be
pramod <cpramod2012@gmail.com>
2015-06-11 19:47:50 +0545

    responsive

--- application/views/frontend/senior_caregiver/left_navbar.php
+++ application/views/frontend/senior_caregiver/left_navbar.php
@@ -2,7 +2,7 @@
 			<h3>					
 			     <h3><?php echo ($this->uri->segment(1) == 'caregivers' && $this->uri->segment(2) == 'workers-staff-for-senior-care-facility') ? 'Workers/Staff for senior care facility' : 'Special needs caregiver';?></h3>					
 			</h3>  			
-	  		<div class="left-search-panel">
+	  		<div class="left-search-panel col-lg-3 col-md-3 col-sm-4 col-xs-4">
 	 	<h4>Advanced Search</h4>
 	 	<form method="post" id="left-nav" action="">
  			<div class="select-services">
