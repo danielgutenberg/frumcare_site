b30f206a5b407779050f3d98b181664d7555187c
pramod <cpramod2012@gmail.com>
2015-06-11 19:52:41 +0545

    responsive

--- application/views/frontend/careseeker_cleaningcompany/left_navbar.php
+++ application/views/frontend/careseeker_cleaningcompany/left_navbar.php
@@ -1,6 +1,6 @@
 <?php echo $this->breadcrumbs->show();?>
 			<h3><?php echo "Works for Cleaning Company";?></h3>  			
-	  		<div class="left-search-panel col-lg-3 col-md-3 col-sm-4 col-xs-4">
+	  		<div class="left-search-panel">
 	 	<h4>Advanced Search</h4>
 	 	<form method="post" id="left-nav" action="">
 	 		
