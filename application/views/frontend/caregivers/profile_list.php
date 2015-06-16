f9fe419c044e9959032051f689b06c7fcae1a319
pramod <cpramod2012@gmail.com>
2015-06-16 14:27:53 +0545

    responsive

--- application/views/frontend/caregivers/profile_list.php
+++ application/views/frontend/caregivers/profile_list.php
@@ -138,8 +138,7 @@
 					 	<?php }?>
 
 					 	
-					</div>
-					<div class="profile-activities">
+
 						<?php if($data['job_type'] == 1){ ?>
 					 	<li> Available full time</li>
 					 	<?php }?>
