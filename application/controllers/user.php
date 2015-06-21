514590341783cbae3330c6ca8fef3f8cd2b334ea
danielguten <danielguten@gmail.com>
2015-06-21 13:29:14 +0000

    remove redirect

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1258,7 +1258,15 @@ class User extends CI_Controller
             $this->db->where('id',$id);
             $this->db->insert('tbl_searchhistory', array('createAlert' => 1));
             $this->session->set_flashdata('info', 'Alert Created Successfully');
-            // redirect('user/searches',true);
+            $current_user = $this->session->userdata['current_user'];
+
+        $data = array(
+            'title'         => 'Search History',
+            'main_content'  => 'frontend/user/viewhistory',
+            'record'        => $this->user_model->getHistory($current_user)
+        );
+
+        $this->load->view(FRONTEND_TEMPLATE,$data);
       }
       
       public function removealert($id)
