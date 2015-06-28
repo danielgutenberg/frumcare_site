c60a7bbb6ca8db972aaa1afe824cf23889f10143
pramod <cpramod2012@gmail.com>
2015-06-28 22:42:43 +0545

    modernizer addition on head

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -43,7 +43,7 @@
     <link rel="icon" href="<?php echo site_url("img/favicon.ico")?>" type="image/x-icon">
     <script src="<?php echo base_url('js/jquery-1.11.1.min.js') ?>"></script>
     <script src="<?php echo base_url('js/jquery.validate.js') ?>"></script>
-    <script src="<?php echo base_url('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js') ?>"></script>
+
     <script src="<?php echo base_url('js/jgrowl.js') ?>"></script>
     <script src="<?php echo base_url('js/vendor/bootstrap.min.js') ?>"></script>
     <script src="<?php echo base_url('js/select.js') ?>"></script>
@@ -52,6 +52,7 @@
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css"/><!--for datepicker-->
     <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script><!--for datepicker-->
     <link href="<?php echo site_url();?>css/jquery-ui.css" type="text/css" rel="stylesheet">
+        <script src="<?php echo base_url('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js') ?>"></script>
 
     <script>
         $("#textbox1").ready(function(){        
