318b4d7db5b69a2cb0c087e754aeea0a78947aae
danielguten <danielguten@gmail.com>
2015-06-28 10:54:29 +0000

    debug

--- application/views/frontend/nursery/left_navbar.php
+++ application/views/frontend/nursery/left_navbar.php
@@ -457,38 +457,38 @@ $(function () {
 </script>
 <script type="text/javascript">
 	$(document).ready(function(){
-	var neighbour = $('.neighbour').val();
-					var caregiverage_from = $('.caregiverage_from').val();
-					var caregiverage_to =  $('.caregiverage_to').val();
-					var gender = $('.gender').is(':checked')?$('input[name=gender_of_caregiver]:checked').val():'';
-                    var willing = $('.willing').is(':checked')?$('input[name=willing]:checked').val():'';
-					var lang = $('.lang:checked').map(function(_, el) {
-			        	return $(el).val();
-			    	}).get();
-			    	var observance = $('.observance:checked').map(function(_, el) {
-			        	return $(el).val();
-			    	}).get();
-			    	 var age_group = $('.age_group:checked').map(function(_, el) {
-			        	return $(el).val();
-			    	}).get();
-                    var smoker = $('.smoker').is(':checked') ? $('input[name=smoker]:checked').val():'';
-			    	var care_type = $( ".careType option:selected" ).val();
-					var data = "neighbour="+neighbour+"&caregiverage_from="+caregiverage_from+"&caregiverage_to="+caregiverage_to+"&languages="+lang+"&observance="+observance+"&age_group="+age_group+"&gender="+gender+"&care_type="+care_type+"&willing="+willing+"&smoker="+smoker
-			    	console.log(data)
-			    	$.ajax({
-			    		type:"get",
-			    		url:"<?php echo site_url();?>nursery/search",
-			    		data:data,
-			    		success:function(done){
-                                 $(".searchloader").fadeOut("fast");
-							var json = jQuery.parseJSON(done);
- 							var pagenum = json.num;
- 							var pagedata = json.userdatas;
-							$('#list_container').html(pagedata);
-							$('#total').text(json.total);
-                            $('.navigations').html(json.pagination);
-			    		}
-			    	});
+	// var neighbour = $('.neighbour').val();
+	// 				var caregiverage_from = $('.caregiverage_from').val();
+	// 				var caregiverage_to =  $('.caregiverage_to').val();
+	// 				var gender = $('.gender').is(':checked')?$('input[name=gender_of_caregiver]:checked').val():'';
+ //                   var willing = $('.willing').is(':checked')?$('input[name=willing]:checked').val():'';
+	// 				var lang = $('.lang:checked').map(function(_, el) {
+	// 		        	return $(el).val();
+	// 		    	}).get();
+	// 		    	var observance = $('.observance:checked').map(function(_, el) {
+	// 		        	return $(el).val();
+	// 		    	}).get();
+	// 		    	 var age_group = $('.age_group:checked').map(function(_, el) {
+	// 		        	return $(el).val();
+	// 		    	}).get();
+ //                   var smoker = $('.smoker').is(':checked') ? $('input[name=smoker]:checked').val():'';
+	// 		    	var care_type = $( ".careType option:selected" ).val();
+	// 				var data = "neighbour="+neighbour+"&caregiverage_from="+caregiverage_from+"&caregiverage_to="+caregiverage_to+"&languages="+lang+"&observance="+observance+"&age_group="+age_group+"&gender="+gender+"&care_type="+care_type+"&willing="+willing+"&smoker="+smoker
+	// 		    	console.log(data)
+	// 		    	$.ajax({
+	// 		    		type:"get",
+	// 		    		url:"<?php echo site_url();?>nursery/search",
+	// 		    		data:data,
+	// 		    		success:function(done){
+ //                                $(".searchloader").fadeOut("fast");
+	// 						var json = jQuery.parseJSON(done);
+ //							var pagenum = json.num;
+ //							var pagedata = json.userdatas;
+	// 						$('#list_container').html(pagedata);
+	// 						$('#total').text(json.total);
+ //                           $('.navigations').html(json.pagination);
+	// 		    		}
+	// 		    	});
 	
 	
 	var $myDialog = $('<div></div>')
