923937189a822753467d4352323f40d3fb00f8bb
danielguten <danielguten@gmail.com>
2015-06-28 14:12:51 +0000

    format filters for caregivers

--- application/views/frontend/common_caregiver.php
+++ application/views/frontend/common_caregiver.php
@@ -179,7 +179,8 @@
 <div class="right-caregivers col-lg-9 col-md-9 col-sm-9 col-xs-12">
     <br />
     <div class="searchloader" style="display:none"></div>		
-    Showing results for <a href="javascript:void(0);" class="showgeolocation" id="showgeolocation1"><?php echo $location ?></a>        
+    Find a <?php $this->load->view('frontend/common/left_nav_title')?> Job <br>
+    Near <a href="javascript:void(0);" class="showgeolocation" id="showgeolocation1"><?php echo $location ?></a>        
     within            
     <select name="sort_by_miles" id="sort_by_miles">        
         <option value="1">1 Miles</option>
