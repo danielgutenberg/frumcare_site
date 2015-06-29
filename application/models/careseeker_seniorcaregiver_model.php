af8e75eba10f6576f9f72d76df1c5b6ba7502757
danielguten <danielguten@gmail.com>
2015-06-29 15:36:13 +0000

    set correct care type for senior caregiver search

--- application/models/careseeker_seniorcaregiver_model.php
+++ application/models/careseeker_seniorcaregiver_model.php
@@ -29,7 +29,7 @@
 		}
         public function search($postdata,$latitude,$longitude){
    	    
-			$sql  = "select *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance  from tbl_user left outer join tbl_userprofile on tbl_user.id = tbl_userprofile.user_id where tbl_user.status = 1 and tbl_userprofile.profile_status = 1 and tbl_userprofile.care_type=19 ";            
+			$sql  = "select *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance  from tbl_user left outer join tbl_userprofile on tbl_user.id = tbl_userprofile.user_id where tbl_user.status = 1 and tbl_userprofile.profile_status = 1 and tbl_userprofile.care_type=20 ";            
 			if(!empty($postdata['gender']) && $postdata['gender'] !='undefined'){				
                 $sql .= " and tbl_user.gender=".$postdata['gender'];
             }
