60894bff94e940b69bceae54bdcdfbad38ba757b
danielguten <danielguten@gmail.com>
2015-06-29 11:56:17 +0000

    debug

--- application/views/frontend/errand_runner/left_navbar.php
+++ application/views/frontend/errand_runner/left_navbar.php
@@ -382,10 +382,12 @@ $(function () {
 		  	var vehicle = $('.vehicle').is(':checked') ? $('.vehicle').val(): '';
 		    var available = $('.short_notice').is('.checked')?$('.short_notice').val():'';
             var start_date = $("#textbox1").val()?$("#textbox1").val():'';
+            var data = "&caregiverage_from="+caregiverage_from+"&caregiverage_to="+caregiverage_to+"&gender="+gender+"&language="+lang+"&observance="+observance+"&min_exp="+min_exp+"&availability="+availability+"&driver_license="+driver_license+"&vehicle="+vehicle+"&available="+available+"&smoker="+smoker+"&start_date="+start_date
+            console.log(data)
 				$.ajax({
 					type:"get",
 					url:"<?php echo site_url();?>errand_runner/search",
-					data:"&caregiverage_from="+caregiverage_from+"&caregiverage_to="+caregiverage_to+"&gender="+gender+"&language="+lang+"&observance="+observance+"&min_exp="+min_exp+"&availability="+availability+"&driver_license="+driver_license+"&vehicle="+vehicle+"&available="+available+"&smoker="+smoker+"&start_date="+start_date,
+					data:data,
 					success:function(done){
 							$(".searchloader").fadeOut("fast");
 							var json = jQuery.parseJSON(done);
