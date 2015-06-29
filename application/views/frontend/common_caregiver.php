a7ac9fb3b01dfdf95b9116e26079dee7cdbff7fb
danielguten <danielguten@gmail.com>
2015-06-29 11:52:01 +0000

    add in missing closing bracket

--- application/views/frontend/common_caregiver.php
+++ application/views/frontend/common_caregiver.php
@@ -179,7 +179,7 @@
 <div class="right-caregivers col-lg-9 col-md-9 col-sm-9 col-xs-12">
     <br />
     <div class="searchloader" style="display:none"></div>		
-    Find a <?php $this->load->view('frontend/common/left_nav_title'); if($s1 == 'jobs') {echo 'Job'?>  <br>
+    Find a <?php $this->load->view('frontend/common/left_nav_title'); if($s1 == 'jobs') {echo 'Job'}?>  <br>
     Near <a href="javascript:void(0);" class="showgeolocation" id="showgeolocation1"><?php echo $location ?></a>        
     within            
     <select name="sort_by_miles" id="sort_by_miles">        
