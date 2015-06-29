f4b9ee1feeee96047433d07015db87d6fd5e8418
danielguten <danielguten@gmail.com>
2015-06-29 19:59:03 +0000

    debug

--- application/controllers/babysitter.php
+++ application/controllers/babysitter.php
@@ -15,6 +15,7 @@ class BabySitter extends CI_Controller{
 	}
 
 	public function index(){
+        echo 'hello';
         $item_per_page = 15;
         $option = "distance";
         $account_category = 1;
