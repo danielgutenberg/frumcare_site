c7d7096eb0797e317cf78d4b8f0fbe873d64ab2f
danielguten <danielguten@gmail.com>
2015-07-02 22:00:47 +0000

    send updated details in edit approval email

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1298,25 +1298,7 @@ class User extends CI_Controller
       }//CODE BY CHAND
         
         
-    public function job_or_profile(){
-        $ac = $this->session->userdata('account_category');
-        $oc = $this->session->userdata('organization_care'); 
-        if($ac == 1){ 
-            $profile1='Profile';
-        }
-        if($ac ==2){ 
-            $profile1='Job';
-        }
-        if($ac=3){
-            if($oc ==1){
-                $profile1='Profile';
-            }
-            if($oc==2){
-                $profile1='Job';
-            }
-        }
-        return $profile1;
-    }    
+       
       public function searches(){
         $this->breadcrumbs->push('My Searches', site_url().'#');
         $this->breadcrumbs->unshift('My Account', base_url().'user/dashboard');
