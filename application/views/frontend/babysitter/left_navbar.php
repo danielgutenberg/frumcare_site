6c5aeaa345d52fa81122e4759ffc4be6c7152fce
danielguten <danielguten@gmail.com>
2015-07-03 19:25:41 +0000

    fix babysitter caregiver filtering

--- application/views/frontend/babysitter/left_navbar.php
+++ application/views/frontend/babysitter/left_navbar.php
@@ -124,7 +124,7 @@
 		 		<label>When you need care</label>
 		 		<div class="checkbox first"><input type="checkbox" class="availability" value="Immediate">Immediate</div>
 		 		<div class="checkbox full"><input type="checkbox" id="chkbox1" value="Start Date">Start Date<input type="text" id="textbox1"/></div>
-		 		<div class="checkbox"><input type="checkbox" class="availability" value="Occasionally">Occasionally</div>
+		 		<div class="checkbox"><input type="checkbox" class="availability" value="Occassionally">Occasionally</div>
 		 		<div class="checkbox"><input type="checkbox" class="availability" value="Regularly">Regularly</div>
 		 		<div class="checkbox"><input type="checkbox" class="availability" value="Morning">Morning</div>
 		 		<div class="checkbox"><input type="checkbox" class="availability" value="Afternoon">Afternoon</div>
