ad2fa27d6ec42f4d427e0485194c879be550541d
danielguten <danielguten@gmail.com>
2015-07-05 23:45:53 +0000

    fix careseeker nanny filtering

--- application/views/frontend/careseeker_nanny/left_navbar.php
+++ application/views/frontend/careseeker_nanny/left_navbar.php
@@ -10,7 +10,7 @@
 	 			<label>Nanny Type</label>
 	 			<div class="checkbox"><input type="checkbox" value="Live In" class="looking_to_work">Live In</div>
 	 			<div class="checkbox"><input type="checkbox" value="Live Out" class="looking_to_work">Live Out</div>
-                <div class="checkbox"><input type="checkbox" value="Any" class="looking_to_work">Any</div>
+                <div class="checkbox"><input type="checkbox" value="" class="looking_to_work">Any</div>
 	 		</div>
                 <div class="select-services">
 	 			<label>Number of children you can care for</label>
@@ -21,7 +21,7 @@
 	 				<option value="3">3</option>
 	 				<option value="4">4</option>
 	 				<option value="5">5</option>
-	 				<option value="5+">5+</option>
+	 				<option value="6">5+</option>
 	 			</select>
 	 		</div>
             <div>
@@ -37,12 +37,12 @@
                 <div class="checkbox"><input type="checkbox" value="1-3" name="age_group[]"  class="age_group"> 1 to 3 years</div>
                 <div class="checkbox"><input type="checkbox" value="3-5" name="age_group[]"  class="age_group"> 3 to 5 years</div>
                 <div class="checkbox"><input type="checkbox" value="6-11" name="age_group[]"  class="age_group"> 6 to 11 years</div>
-                <div class="checkbox"><input type="checkbox" value="12+" name="age_group[]"  class="age_group"> 12+ years</div>
+                <div class="checkbox"><input type="checkbox" value="13" name="age_group[]"  class="age_group"> 12+ years</div>
 	 		</div>
              <div>
 		 		<label>Job Hours</label>
 		 		<div class="checkbox"><input type="checkbox" class="availability" value="One Time">One Time</div>
-                <div class="checkbox"><input type="checkbox" class="availability" value="Occasionally">Occasionally</div>
+                <div class="checkbox"><input type="checkbox" class="availability" value="Occassionally">Occasionally</div>
                 <div class="checkbox"><input type="checkbox" class="availability" value="Regularly">Regularly</div>
                 <div class="checkbox"><input type="checkbox" class="availability" value="Asap">Asap</div>
 		 		<div class="checkbox full"><input type="checkbox" id="chkbox1" value="Start Date">Start Date<input type="text" id="textbox1"/></div>		 				 				 		
