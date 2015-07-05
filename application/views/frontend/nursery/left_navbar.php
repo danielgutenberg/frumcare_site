1ed28fcde3868ceed13bf015b9d8206f94c7eeef
danielguten <danielguten@gmail.com>
2015-07-05 20:11:37 +0000

    fix nursery caregiver filtering

--- application/views/frontend/nursery/left_navbar.php
+++ application/views/frontend/nursery/left_navbar.php
@@ -58,7 +58,7 @@
                 <div class="checkbox"><input type="checkbox" value="1-3" name="age_group[]"  class="age_group"> 1 to 3 years</div>
                 <div class="checkbox"><input type="checkbox" value="3-5" name="age_group[]"  class="age_group"> 3 to 5 years</div>
                 <div class="checkbox"><input type="checkbox" value="6-11" name="age_group[]"  class="age_group"> 6 to 11 years</div>
-                <div class="checkbox"><input type="checkbox" value="12+" name="age_group[]"  class="age_group"> 12+ years</div>
+                <div class="checkbox"><input type="checkbox" value="13" name="age_group[]"  class="age_group"> 12+ years</div>
 	 		</div>
 	 	
 
