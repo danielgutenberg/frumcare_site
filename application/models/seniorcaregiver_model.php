ed6e08f874e8df1239a46e86a98aa2f76253153c
danielguten <danielguten@gmail.com>
2015-07-05 21:28:52 +0000

    fix senior caregiver filtering?

--- application/models/seniorcaregiver_model.php
+++ application/models/seniorcaregiver_model.php
@@ -76,7 +76,7 @@
 				}
 
 				if($min_exp!=''){
-					$sql .= " and tbl_userprofile.experience = $min_exp";
+					$sql .= " and tbl_userprofile.experience >= $min_exp";
 				}
 
 				if($availability!=''){
