7fc638021cc67b983f94f70fb933434699c3a6e0
danielguten <danielguten@gmail.com>
2015-07-06 03:17:32 +0000

    fix careseeker errand runner filtering

--- application/views/frontend/careseeker_errandrunner/left_navbar.php
+++ application/views/frontend/careseeker_errandrunner/left_navbar.php
@@ -24,7 +24,7 @@
 	 			<label>Gender of helper wanted</label>
 	 			<div class="radio-half"><input type="radio" name="gender_of_caregiver" value="1" class="gender_of_caregiver">Male</div>
 	 			<div class="radio-half"><input type="radio" name="gender_of_caregiver" value="2" class="gender_of_caregiver"> Female</div>
-	 			<div class="radio-half"><input type="radio" name="gender_of_caregiver" value="3" class="gender_of_caregiver"> Any</div>
+	 			<div class="radio-half"><input type="radio" name="gender_of_caregiver" value="" class="gender_of_caregiver"> Any</div>
 	 		</div>
 
 	 	 <div class="select-services">
