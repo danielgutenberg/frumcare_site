dd073231eb8015ae85fa4e52c71d975d57422f98
danielguten <danielguten@gmail.com>
2015-07-06 02:30:57 +0000

    fix careseeker special needs filtering

--- application/models/careseeker_specialneedscaregiver_model.php
+++ application/models/careseeker_specialneedscaregiver_model.php
@@ -31,7 +31,7 @@
    	    
 			$sql  = "select *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance  from tbl_user left outer join tbl_userprofile on tbl_user.id = tbl_userprofile.user_id where tbl_user.status = 1 and tbl_userprofile.profile_status = 1 and tbl_userprofile.care_type=22";            
 			if(!empty($postdata['gender']) && $postdata['gender'] !='undefined'){				
-                $sql .= " and tbl_userprofile.gender_of_careseekr=".$postdata['gender'];
+                $sql .= " and tbl_userprofile.gender_of_careseeker=".$postdata['gender'];
             }
            	if(!empty($postdata['gender_of_caregiver']) && $postdata['gender_of_caregiver'] !='undefined'){				
                 $sql .= " and tbl_userprofile.gender_of_caregiver=".$postdata['gender_of_caregiver'];
