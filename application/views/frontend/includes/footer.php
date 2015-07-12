6b1f4c24b1781d232a005d9c04bd1281f61abf49
pramod <cpramod2012@gmail.com>
2015-07-12 18:06:14 +0545

    analytics installation

--- application/views/frontend/includes/footer.php
+++ application/views/frontend/includes/footer.php
@@ -115,17 +115,7 @@
 <script src="<?php echo base_url('js/my_js.js') ?>"></script>
 <script src="<?php echo base_url('js/main.js') ?>"></script>
 
-<script>
-    var _gaq = [
-        ['_setAccount', 'UA-XXXXX-X'],
-        ['_trackPageview']
-    ];
-    (function (d, t) {
-        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-        g.src = '//www.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g, s)
-    }(document, 'script'));
-</script>
+
 
 <script>
     $(function(){
