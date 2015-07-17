f8b6782dfe93ce16f5f4a50ed0a20f1f4a63de96
danielguten <danielguten@gmail.com>
2015-07-17 20:39:44 +0000

    debug

--- application/models/common_care_model.php
+++ application/models/common_care_model.php
@@ -37,7 +37,6 @@ class Common_care_model extends CI_Model
             $sql.=" having distance <= $distance";
         }
         $sql.= " order by $option $order_type";
-        print_r($sql);
         $query = $this->db->query($sql);
         if($query){
             return $query->result_array();
