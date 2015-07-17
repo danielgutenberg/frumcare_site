1344103ea323ca2c94496380eb7078313ea7b98c
danielguten <danielguten@gmail.com>
2015-07-17 16:20:11 +0000

    fix caregivers all query

--- application/models/babysitter_model.php
+++ application/models/babysitter_model.php
@@ -37,7 +37,7 @@
 		}
 		
 		public function searchAll($postdata,$latitude,$longitude){
-			$sql  = "select *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance from tbl_user left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type left outer join tbl_userprofile on tbl_user.id = tbl_userprofile.user_id where tbl_user.status = 1 and tbl_userprofile.profile_status = 1 and tbl_care.service_type = 1";            			
+			$sql  = "select *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance from tbl_user left outer join tbl_userprofile on tbl_user.id = tbl_userprofile.user_id left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type where tbl_user.status = 1 and tbl_userprofile.profile_status = 1 and tbl_care.service_type = 1";            			
             if($postdata['gender'] && $postdata['gender'] != 3 ){                
 			     $sql .=" and tbl_user.gender=".$postdata['gender'];
 			}
