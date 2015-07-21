e9537ef6195993b8746e144bc6c29694610802ac
danielguten <danielguten@gmail.com>
2015-07-21 19:17:46 +0000

    fix titles on left navbar

--- application/views/frontend/errand_runner/left_navbar.php
+++ application/views/frontend/errand_runner/left_navbar.php
@@ -1,6 +1,6 @@
 <?php echo $this->breadcrumbs->show();?>
 			<h3>
-					<?php echo ucwords(str_replace('_', ' ', segment(1))); ?>
+					<?php echo 'Errand runner / odd jobs / personal assistant / driver'; ?>
 			</h3>  			
 	  		<div class="left-search-panel col-lg-3 col-md-3 col-sm-3 col-xs-12">
 	 	<h4>Advanced Search</h4>
