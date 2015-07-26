6cdd41c3fc1db5fae2736379eec1ea41a6cc614d
danielguten <danielguten@gmail.com>
2015-07-26 07:56:35 +0000

    debug

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1387,6 +1387,7 @@ class User extends CI_Controller
             $this->breadcrumbs->unshift('My Account', base_url().'user/dashboard');
             $this->db->where('id',$id);
             $this->db->update('tbl_searchhistory', $insert);
+            sleep(2);
             $this->session->set_flashdata('info', 'Alert Created Successfully');
             redirect('user/searches', 'refresh');
       }
