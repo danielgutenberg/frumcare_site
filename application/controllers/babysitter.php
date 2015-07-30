222d85190bcd55525c5d46b406c6774bdf169948
danielguten <danielguten@gmail.com>
2015-07-30 11:42:50 +0000

    save location on page back in browser

--- application/controllers/babysitter.php
+++ application/controllers/babysitter.php
@@ -151,88 +151,93 @@ class BabySitter extends CI_Controller{
 	
 	public function searchAll(){
 		$limit = 15;
-		if(check_user()){
-                    $locationdetails = $this->common_model->getMyLocation(check_user());
-                    if($locationdetails){
-                        $latitude = ($locationdetails[0]['lat']);
-                        $longitude = ($locationdetails[0]['lng']);
-                        $location =  isset($locationdetails[0]['location'])?$locationdetails[0]['location']:'your city';
-                    }
-                    if (!$latitude) {
-                        $ipdata = $this->common_model->getIPData($this->ipaddress);
-                        if(is_array($ipdata)){
-                            $latitude = ($ipdata['lat']);
-                            $longitude = ($ipdata['lon']);
-                            $location = isset($ipdata['city'])?$ipdata['city']:'your city';
-                        }
-                    }
-            
+		if(isset($_POST['lat']) && isset($_POST['lng'])){
+            $latitude = $this->input->post('lat',true);
+            $longitude = $this->input->post('lng',true);                      
+        } else {
+    		if(check_user()){
+                $locationdetails = $this->common_model->getMyLocation(check_user());
+                if($locationdetails){
+                    $latitude = ($locationdetails[0]['lat']);
+                    $longitude = ($locationdetails[0]['lng']);
+                    $location =  isset($locationdetails[0]['location'])?$locationdetails[0]['location']:'your city';
                 }
-                else{
+                if (!$latitude) {
                     $ipdata = $this->common_model->getIPData($this->ipaddress);
-                        if(is_array($ipdata)){
-                            $latitude = ($ipdata['lat']);
-                            $longitude = ($ipdata['lon']);
-                            $location = isset($ipdata['city'])?$ipdata['city']:'your city';
-                        }
-                }
-			$postdata['neighbor'] 			= $this->input->get('neighbour',true);
-			$postdata['gender']   			= $this->input->get('gender',true);
-			$postdata['smoker']	  			= $this->input->get('smoker',true);
-			$postdata['language'] 			= $this->input->get('lang',true);
-			$postdata['observance'] 		= $this->input->get('observance',true);
-			$postdata['number_of_children'] = $this->input->get('number_of_children',true);
-			$postdata['morenum']			= $this->input->get('morenum',true);
-			$postdata['age_group']			= $this->input->get('age_group',true);
-			$postdata['looking_to_work']	= $this->input->get('looking_to_work',true);
-			$postdata['year_experience']	= $this->input->get('year_experience',true);
-			$postdata['training']			= $this->input->get('training',true);
-			$postdata['availability']		= $this->input->get('availability',true);
-			$postdata['driver_license']		= $this->input->get('driver_license',true);
-			$postdata['vehicle']			= $this->input->get('vehicle',true);
-			$postdata['pick_up_child']		= $this->input->get('pick_up_child',true);
-			$postdata['cook']				= $this->input->get('cook',true);
-			$postdata['basic_housework']    = $this->input->get('basic_housework',true);
-            $postdata['homework_help']		= $this->input->get('homework_help',true);
-			$postdata['sick_child_care']	= $this->input->get('sick_child_care',true);
-			$postdata['on_short_notice']	= $this->input->get('on_short_notice',true);
-            $postdata['caregiverage_from']  = $this->input->get('caregiverage_from',true);
-            $postdata['caregiverage_to']    = $this->input->get('caregiverage_to',true);
-            $postdata['start_date']         = $this->input->get('start_date',true);
-            $postdata['care_type']          = $this->input->get('care_type',true);
-
-            $result = $this->babysitter->searchAll($postdata,$latitude,$longitude);
-
-			if(is_array($result))
-				$total = count($result);
-			else
-				$total = 0;
-				
-			$pages = ceil($total/$limit);        
-            $pagination	= '';
-            if($pages > 1){	
-            	$pagination .= '<a href="#" class="paginate_click in-active" id="previous">previous</a>';
-            	for($i = 1; $i<=$pages; $i++)
-            	{
-            		
-            		if($i==1){
-                        $pagination .= ' <a href="#" class="paginate_click active" id="'.$i.'-page" >'.$i.'</a> ';
-                    }else{
-                        $pagination .= ' <a href="#" class="paginate_click in-active" id="'.$i.'-page">'.$i.'</a> ';   
+                    if(is_array($ipdata)){
+                        $latitude = ($ipdata['lat']);
+                        $longitude = ($ipdata['lon']);
+                        $location = isset($ipdata['city'])?$ipdata['city']:'your city';
                     }
-                    
-            	}
-            	$pagination .= '<a href="#" class="paginate_click in-active" id="next">next</a></div>';
+                }
+        
             }
-			$result = array_slice($result, 0 , $limit);
-			$userlogs             = $this->user_model->getUserLog();            
-            $merge['userdatas']   = $this->load->view('frontend/common_profile_list', array('userdatas'=>$result,'userlogs'=>$userlogs,'location'=>$location), true); 
-            $merge['num']         =  ceil($total/$limit); 
-            $merge['total']       = $total;
-            $merge['pagination']  = $pagination;
-            echo json_encode($merge);
-            exit();
-		//}
+            else{
+                $ipdata = $this->common_model->getIPData($this->ipaddress);
+                    if(is_array($ipdata)){
+                        $latitude = ($ipdata['lat']);
+                        $longitude = ($ipdata['lon']);
+                        $location = isset($ipdata['city'])?$ipdata['city']:'your city';
+                    }
+            }
+        }
+		$postdata['neighbor'] 			= $this->input->get('neighbour',true);
+		$postdata['gender']   			= $this->input->get('gender',true);
+		$postdata['smoker']	  			= $this->input->get('smoker',true);
+		$postdata['language'] 			= $this->input->get('lang',true);
+		$postdata['observance'] 		= $this->input->get('observance',true);
+		$postdata['number_of_children'] = $this->input->get('number_of_children',true);
+		$postdata['morenum']			= $this->input->get('morenum',true);
+		$postdata['age_group']			= $this->input->get('age_group',true);
+		$postdata['looking_to_work']	= $this->input->get('looking_to_work',true);
+		$postdata['year_experience']	= $this->input->get('year_experience',true);
+		$postdata['training']			= $this->input->get('training',true);
+		$postdata['availability']		= $this->input->get('availability',true);
+		$postdata['driver_license']		= $this->input->get('driver_license',true);
+		$postdata['vehicle']			= $this->input->get('vehicle',true);
+		$postdata['pick_up_child']		= $this->input->get('pick_up_child',true);
+		$postdata['cook']				= $this->input->get('cook',true);
+		$postdata['basic_housework']    = $this->input->get('basic_housework',true);
+        $postdata['homework_help']		= $this->input->get('homework_help',true);
+		$postdata['sick_child_care']	= $this->input->get('sick_child_care',true);
+		$postdata['on_short_notice']	= $this->input->get('on_short_notice',true);
+        $postdata['caregiverage_from']  = $this->input->get('caregiverage_from',true);
+        $postdata['caregiverage_to']    = $this->input->get('caregiverage_to',true);
+        $postdata['start_date']         = $this->input->get('start_date',true);
+        $postdata['care_type']          = $this->input->get('care_type',true);
+
+        $result = $this->babysitter->searchAll($postdata,$latitude,$longitude);
+
+		if(is_array($result))
+			$total = count($result);
+		else
+			$total = 0;
+			
+		$pages = ceil($total/$limit);        
+        $pagination	= '';
+        if($pages > 1){	
+        	$pagination .= '<a href="#" class="paginate_click in-active" id="previous">previous</a>';
+        	for($i = 1; $i<=$pages; $i++)
+        	{
+        		
+        		if($i==1){
+                    $pagination .= ' <a href="#" class="paginate_click active" id="'.$i.'-page" >'.$i.'</a> ';
+                }else{
+                    $pagination .= ' <a href="#" class="paginate_click in-active" id="'.$i.'-page">'.$i.'</a> ';   
+                }
+                
+        	}
+        	$pagination .= '<a href="#" class="paginate_click in-active" id="next">next</a></div>';
+        }
+		$result = array_slice($result, 0 , $limit);
+		$userlogs             = $this->user_model->getUserLog();            
+        $merge['userdatas']   = $this->load->view('frontend/common_profile_list', array('userdatas'=>$result,'userlogs'=>$userlogs,'location'=>$location), true); 
+        $merge['num']         =  ceil($total/$limit); 
+        $merge['total']       = $total;
+        $merge['pagination']  = $pagination;
+        echo json_encode($merge);
+        exit();
+	//}
 	}
 
    public function searchbylocation(){
