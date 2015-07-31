0373e5e0fe8c9498b9b48bdab507df3a08fd242e
danielguten <danielguten@gmail.com>
2015-07-31 10:56:41 +0000

    fix label to age of caregiver istead of caregivers

--- application/views/frontend/all/left_navbar.php
+++ application/views/frontend/all/left_navbar.php
@@ -26,7 +26,7 @@
 
 	 		?>
  			<div>
-	 			<label>Age of Caregivers</label>
+	 			<label>Age of Caregiver</label>
 	 			<div>
 	 			<input type="text" name="caregiverage_from" value="" placeholder="FROM" style="width:33%" class="caregiverage_from"> to  
 	 			<input type="text" name="caregiverage_to" value="" placeholder="TO" style="width:33%" class="caregiverage_to">
