91eab4b22d5294ace7f58533cfb51ffd4acdf9d3
danielguten <danielguten@gmail.com>
2015-08-02 20:56:53 +0000

    fix seniorcarefacility careseeker filtering

--- application/controllers/careseeker_seniorcarefacility.php
+++ application/controllers/careseeker_seniorcarefacility.php
@@ -163,7 +163,17 @@ class Careseeker_seniorcarefacility extends CI_Controller{
 
 
     public function search(){
-		$limit = 15;	
+		$page = $this->input->get('pagenum',true);
+	    $offset = 0;
+	    if ($page > 1) {
+	        $offset = ($page - 1) * 15;
+	    }
+		$limit = 15;
+            $latitude = $this->input->get('lat',true);
+            $longitude = $this->input->get('lng',true);
+            $location = $this->input->get('location',true);
+		$limit = 15;
+		if (!$latitude || !$longitude || !$location) {	
 		  if(check_user()){
                     $locationdetails = $this->common_model->getMyLocation(check_user());
                     if($locationdetails){
@@ -188,23 +198,41 @@ class Careseeker_seniorcarefacility extends CI_Controller{
                             $longitude = ($ipdata['lon']);
                             $location = isset($ipdata['city'])?$ipdata['city']:'your city';
                         }
-                }   
+                }  
+		}
 			$postdata['neighbor'] 			= $this->input->get('neighbour',true);
 			$postdata['looking_to_work']	= $this->input->get('looking_to_work',true);
 			$postdata['rate']               = $this->input->get('rate',true);
             $postdata['rate_type']          = $this->input->get('rate_type',true);
             
-            $res = $this->seniorcarefacility->search($postdata,$latitude,$longitude);
-			if(is_array($res))
-				$total = count($res);
+            $result = $this->seniorcarefacility->search($postdata,$latitude,$longitude);
+			if(is_array($result))
+				$total = count($result);
 			else
 				$total = 0;
-			$location = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
-			$userlogs             	= $this->user_model->getUserLog();
-            $merge['userdatas']   	= $this->load->view('frontend/common_profile_list', array('userdatas'=>$res,'userlogs'=>$userlogs,'location'=>$location), true);
-            $total_rows           	= $total;
-            $merge['num']         	= ceil($total_rows/@$limit); 
-            $merge['total']       	= $total_rows;  
+			$pages = ceil($total/$limit);        
+            $pagination	= '';
+            if($pages > 1){	
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="previous">previous</a>';
+            	for($i = 1; $i<=$pages; $i++)
+            	{
+            		
+            		if($i==$page){
+                        $pagination .= ' <a href="#" class="paginate_click active" id="'.$i.'-page" >'.$i.'</a> ';
+                    }else{
+                        $pagination .= ' <a href="#" class="paginate_click in-active" id="'.$i.'-page">'.$i.'</a> ';   
+                    }
+                    
+            	}
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="next">next</a></div>';
+            }
+            $locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
+    		$result = array_slice($result, $offset , $limit);$userlogs             	= $this->user_model->getUserLog();
+            $merge['userdatas']   	= $this->load->view('frontend/common_profile_list', array('userdatas'=>$result,'userlogs'=>$userlogs,'location'=>$locationdetails), true);
+            $merge['num']       =  ceil($total/$limit); 
+            $merge['total']     = $total;
+            $merge['pagination']       	= $pagination;
+            $merge['location'] = $location; 
             echo json_encode($merge);
             exit();		
 	}
