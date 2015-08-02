6cc15edd6dee4f21253a87fd6cb028a5775e5fd1
danielguten <danielguten@gmail.com>
2015-08-02 19:40:39 +0000

    fir careseeker babysitter filtering

--- application/controllers/careseeker_babysitter.php
+++ application/controllers/careseeker_babysitter.php
@@ -153,7 +153,17 @@ class Careseeker_babysitter extends CI_Controller{
 
 
     	public function search(){
-		$limit = 15;	
+		$page = $this->input->get('pagenum',true);
+	    $offset = 0;
+	    if ($page > 1) {
+	        $offset = ($page - 1) * 15;
+	    }
+		$limit = 15;
+            $latitude = $this->input->get('lat',true);
+            $longitude = $this->input->get('lng',true);
+            $location = $this->input->get('location',true);
+		$limit = 15;
+		if (!$latitude || !$longitude || !$location) {	
             if(check_user()){
                 $locationdetails = $this->common_model->getMyLocation(check_user());
                 if($locationdetails){
@@ -179,6 +189,7 @@ class Careseeker_babysitter extends CI_Controller{
                     $location = isset($ipdata['city'])?$ipdata['city']:'your city';
                 }
             }
+		}
 			$postdata['neighbor'] 			= $this->input->get('neighbour',true);
 			$postdata['number_of_children'] = $this->input->get('number_of_children',true);
 			$postdata['morenum']			= $this->input->get('morenum',true);
@@ -194,19 +205,46 @@ class Careseeker_babysitter extends CI_Controller{
 				$total = count($res);
 			else
 				$total = 0;
-			$locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
-			$userlogs             	= $this->user_model->getUserLog();
+			$pages = ceil($total/$limit);        
+            $pagination	= '';
+            if($pages > 1){	
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="previous">previous</a>';
+            	for($i = 1; $i<=$pages; $i++)
+            	{
+            		
+            		if($i==$page){
+                        $pagination .= ' <a href="#" class="paginate_click active" id="'.$i.'-page" >'.$i.'</a> ';
+                    }else{
+                        $pagination .= ' <a href="#" class="paginate_click in-active" id="'.$i.'-page">'.$i.'</a> ';   
+                    }
+                    
+            	}
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="next">next</a></div>';
+            }
+            $locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
+    		$result = array_slice($result, $offset , $limit);$userlogs             	= $this->user_model->getUserLog();
             $merge['userdatas']   	= $this->load->view('frontend/common_profile_list', array('userdatas'=>$res,'userlogs'=>$userlogs,'location'=>$locationdetails), true);
-            $total_rows           	= $total;
-            $merge['num']         	= ceil($total_rows/@$limit); 
-            $merge['total']       	= $total_rows;
-            $merge['pagination']       	= '';  
+            
+            $merge['num']       =  ceil($total/$limit); 
+            $merge['total']     = $total;
+            $merge['pagination']       	= $pagination;
+            $merge['location'] = $location;  
             echo json_encode($merge);
             exit();		
 	}
 	
 	public function searchAll(){
-		$limit = 15;	
+		$page = $this->input->get('pagenum',true);
+	    $offset = 0;
+	    if ($page > 1) {
+	        $offset = ($page - 1) * 15;
+	    }
+		$limit = 15;
+            $latitude = $this->input->get('lat',true);
+            $longitude = $this->input->get('lng',true);
+            $location = $this->input->get('location',true);
+		$limit = 15;
+		if (!$latitude || !$longitude || !$location) {	
             if(check_user()){
                 $locationdetails = $this->common_model->getMyLocation(check_user());
                 if($locationdetails){
@@ -232,6 +270,7 @@ class Careseeker_babysitter extends CI_Controller{
                     $location = isset($ipdata['city'])?$ipdata['city']:'your city';
                 }
             }
+		}
 			$postdata['neighbor'] 			= $this->input->get('neighbour',true);
 			$postdata['number_of_children'] = $this->input->get('number_of_children',true);
 			$postdata['morenum']			= $this->input->get('morenum',true);
@@ -247,14 +286,31 @@ class Careseeker_babysitter extends CI_Controller{
 				$total = count($res);
 			else
 				$total = 0;
-			$locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
-			$res = array_slice($res, 0 , 15);
+			$pages = ceil($total/$limit);        
+            $pagination	= '';
+            if($pages > 1){	
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="previous">previous</a>';
+            	for($i = 1; $i<=$pages; $i++)
+            	{
+            		
+            		if($i==$page){
+                        $pagination .= ' <a href="#" class="paginate_click active" id="'.$i.'-page" >'.$i.'</a> ';
+                    }else{
+                        $pagination .= ' <a href="#" class="paginate_click in-active" id="'.$i.'-page">'.$i.'</a> ';   
+                    }
+                    
+            	}
+            	$pagination .= '<a href="#" class="paginate_click in-active" id="next">next</a></div>';
+            }
+            $locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
+    		$result = array_slice($result, $offset , $limit);
 			$userlogs             	= $this->user_model->getUserLog();
             $merge['userdatas']   	= $this->load->view('frontend/common_profile_list', array('userdatas'=>$res,'userlogs'=>$userlogs,'location'=>$locationdetails), true);
-            $total_rows           	= $total;
-            $merge['num']         	= ceil($total_rows/@$limit); 
-            $merge['total']       	= $total_rows;
-            $merge['pagination']       	= '';  
+            
+            $merge['num']       =  ceil($total/$limit); 
+            $merge['total']     = $total;
+            $merge['pagination']       	= $pagination;
+            $merge['location'] = $location;  
             echo json_encode($merge);
             exit();		
 	}
