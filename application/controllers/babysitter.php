9eca2f271ce7978b3567607a8e992c21c5fca316
danielguten <danielguten@gmail.com>
2015-08-02 10:09:22 +0000

    fix nanny caregiver filtering

--- application/controllers/babysitter.php
+++ application/controllers/babysitter.php
@@ -171,7 +171,7 @@ class BabySitter extends CI_Controller{
 			$locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
 			$userlogs           = $this->user_model->getUserLog();            
             $merge['userdatas'] = $this->load->view('frontend/common_profile_list', array('userdatas'=>$result,'userlogs'=>$userlogs,'location'=>$locationdetails), true); 
-            $merge['num']       =  ceil($total_rows/$limit); 
+            $merge['num']       =  ceil($total/$limit); 
             $merge['total']     = $total;
             $merge['pagination']       	= $pagination;
             $merge['location'] = $location;
