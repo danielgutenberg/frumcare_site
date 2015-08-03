53cb76f4fd1284f250ed4174d3e3b1230aadeb6c
danielguten <danielguten@gmail.com>
2015-08-03 09:03:02 +0000

    fix meet ... in details page

--- application/views/frontend/all/left_navbar.php
+++ application/views/frontend/all/left_navbar.php
@@ -541,7 +541,7 @@ $(document).ready(function () {
                         if (json.location) {
                         	$('#locationaddress').text(json.location)
                         }
-                        setTimeout(function(){if ($('.paginate.active').length == 0) {
+                        setTimeout(function(){if ($('.paginate_click.active').length == 0) {
                             $('#1-page').removeClass('in-active');
                             $('#1-page').addClass('active');
                             $('#pagenum').val(1);
