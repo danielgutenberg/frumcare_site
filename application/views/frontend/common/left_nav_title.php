b8ef5714fbb8f8589b4bc296633501c878c8c197
danielguten <danielguten@gmail.com>
2015-08-04 12:52:27 +0000

    debug

--- application/views/frontend/common/left_nav_title.php
+++ application/views/frontend/common/left_nav_title.php
@@ -47,7 +47,7 @@
         <option value="25" <?php if(segment(2) == 'workers-staff-for-childcare-facility'){?> selected="selected" <?php }?>>Workers / Staff for childcare facility</option>
         <option value="26" <?php if(segment(2) == 'workers-staff-for-senior-care-facility'){?> selected="selected" <?php }?> >Workers / Staff for senior care facility</option>
         <option value="27" <?php if(segment(2) == 'workers-staff-for-special-needs-facility'){?> selected="selected" <?php }?>>Workers / Staff for special needs facility</option>
-        <option value="28" <?php if(segment(2) == 'cleaning-household-help-company'){?> selected="selected" <?php } ?>>Workers for cleaning company</option>
+        <option value="28" <?php if(segment(2) == 'workers-for-cleaning-company'){?> selected="selected" <?php } ?>>Workers for cleaning company</option>
     </select>
 <?php } ?>
 
