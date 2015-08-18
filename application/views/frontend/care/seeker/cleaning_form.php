3afc43ceadd160e2d2963086b0d7bef5e2fcf9f8
danielguten <danielguten@gmail.com>
2015-08-18 13:11:23 +0000

    fix format of cleaning help ad page

--- application/views/frontend/care/seeker/cleaning_form.php
+++ application/views/frontend/care/seeker/cleaning_form.php
@@ -78,15 +78,14 @@ $user_detail = get_user(check_user());
 
             <input type="hidden" name="account_type1" value="<?php echo $this->uri->segment(3);?>"/>
             <input type="hidden" name="account_type2" value="<?php echo $this->uri->segment(4);?>"/>
-
-
-<label>Location</label>
-<div id="locationField">
-    <input type="hidden" id="lat" name="lat"/>
-    <input type="hidden" id="lng" name="lng"/> 
-    <input type="text" name="location" class="required" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
-    <span style="color:red;" id="error"> </span>
-</div> 
+            <label>Location</label>
+            <div id="locationField">
+                <input type="hidden" id="lat" name="lat"/>
+                <input type="hidden" id="lng" name="lng"/> 
+                <input type="text" name="location" class="required" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
+                <span style="color:red;" id="error"> </span>
+            </div>
+            </div>
                 <div>
                     <label>Neighborhood / Street</label>
                     <div>
