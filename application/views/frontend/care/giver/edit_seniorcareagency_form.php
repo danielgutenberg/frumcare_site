ddd31d94c72412ed242ec8dcb643061ada5a2404
pramod <cpramod2012@gmail.com>
2015-08-19 20:34:01 +0545

    image approval

--- application/views/frontend/care/giver/edit_seniorcareagency_form.php
+++ application/views/frontend/care/giver/edit_seniorcareagency_form.php
@@ -1,5 +1,5 @@
 <link href="<?php echo site_url();?>css/user.css" rel="stylesheet" type="text/css">
-<?php 
+<?php
 	if($detail){
 		$willing_to_work = explode(',', $detail[0]['willing_to_work']);
 		$hr_rate 		 = $detail[0]['hourly_rate'];
@@ -9,7 +9,7 @@
         $rate = $detail[0]['rate'];
         $rate_type = explode(',',$detail[0]['rate_type']);
         $facility  = $detail[0]['facility_pic'];
-	}    
+	}
 ?>
 <?php $care_type = $this->uri->segment(4);?>
 <div class="container">
@@ -18,15 +18,15 @@
     <div class="dashboard-left float-left">
          <?php $this->load->view('frontend/user/dashboard_nav');?>
     </div>
-    
+
     <div class="dashboard-right float-right">
-    
+
     <form action="<?php echo site_url().'user/update_job_details/'.$care_type;?>" method="post">
         <div class="ad-form-container float-left">
             <div class="top-welcome">
                 <h2 class="step3">Edit Organization Details</h2>
             </div>
-            
+
             <div>
                 <label>Year established</label>
                 <div class="form-field">
@@ -50,7 +50,7 @@
                 <div class="form-field">
                 <div class="checkbox"><input type="checkbox" value="Alz./dementia" name="willing_to_work[]" <?php if(in_array('Alz./dementia',$willing_to_work)){?> checked="checked"<?php }?>> <span>Alz./ dementia</span></div>
                 <div class="checkbox"><input type="checkbox" value="Sight loss" name="willing_to_work[]" <?php if(in_array('Sight loss',$willing_to_work)){?> checked="checked"<?php }?>> <span>Sight loss</span></div>
-                
+
                 <div class="checkbox"><input type="checkbox" value="Hearing loss" name="willing_to_work[]" <?php if(in_array('Hearing loss',$willing_to_work)){?> checked="checked"<?php }?>> <span>Hearing loss</span></div>
                 <div class="checkbox"><input type="checkbox" value="Wheelchair bound" name="willing_to_work[]" <?php if(in_array('Wheelchair bound',$willing_to_work)){?> checked="checked"<?php }?>> <span>Wheelchair bound</span></div>
                 </div>
@@ -60,9 +60,9 @@
                 <label>Cost</label>
                 <div class="form-field">
                    <input type="text" name="rate" value="<?php echo $rate;?>">
-                </div> 
+                </div>
             </div>
-             
+
             <div>
                 <!--<label>Check one or more</label>-->
                 <div class="form-field">
@@ -77,13 +77,13 @@
                 </div>
             </div>
 
-            <?php 
+            <?php
                 $photo_url = site_url("images/plus.png");
                 if(isset($facility)){
                     $photo_url = base_url('images/profile-picture/thumb/'.$facility);
-                } 
-            ?>                   
-                    
+                }
+            ?>
+
                 <div class="upload-photo"  style="display:none;">
                     <h2>Upload photo of facility / organization</h2>
                     <input type="hidden" id="file-name" name="facility_pic" value="<?php echo isset($facility)?>">
@@ -107,7 +107,7 @@
                  <input type="submit" class="btn btn-success" value="Update"/>
             </div>
         </div>
-    </form>    
+    </form>
 </div>
 </div>
 <script type="text/javascript">
@@ -115,72 +115,10 @@
         $('body').removeAttr('onload');
     });
 
-    $('#upload,#output').click(function(e){
-            e.preventDefault();
-            $('#ImageFile').trigger('click');
-     });    
+
 
    $(function(){
-        var files;
 
-        var loader = '<img src="<?php echo site_url("images/loader.gif")?>">';
-        var link = '<?php echo site_url("ad/upload_pp?files")?>';
-
-        $('#ImageFile').on('change',prepareUpload);
-
-        // Grab the files and set them to our variable
-        function prepareUpload(event){
-            files = event.target.files;
-            event.stopPropagation(); // Stop stuff happening
-            event.preventDefault(); // Totally stop stuff happening
-
-            // START A LOADING SPINNER HERE
-
-            // Create a formdata object and add the files
-            var data = new FormData();
-            $.each(files, function(key, value)
-            {
-                data.append(key, value);
-            });
-            $.ajax({
-                url: link,
-                type: 'POST',
-                beforesend: $('.loader').html(loader),
-                data: data,
-                cache: false,
-                dataType: 'json',
-                processData: false, // Don't process the files
-                contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-                success: function(data, textStatus, jqXHR)
-                {
-                    if(typeof data.error === 'undefined')
-                    {
-                        // Success so call function to process the form
-                        if(data.type==1){
-                            $('#output').html(data.html);
-                            $('.loader').html('');
-                            $('#file-name').val(data.files);    
-                        }
-                        else{
-                            $('#output').html(data.files + ' selected');
-                            $('#file-name').val(data.files);
-                        }
-                        
-                    }
-                    else
-                    {
-                        // Handle errors here
-                        console.log('ERRORS: ' + data.error);
-                    }
-                },
-                error: function(jqXHR, textStatus, errorThrown)
-                {
-                    // Handle errors here
-                    console.log('ERRORS: ' + textStatus);
-                    // STOP LOADING SPINNER
-                }
-            });
-        }
 
     //     function prepareUpload(event){
     //      var loader = '<img src="<?php echo site_url("images/loader.gif")?>">';
@@ -215,13 +153,13 @@
     //                 if(data.type==1){
     //                     $('#output').html(data.html);
     //                     $('.loader').html('');
-    //                     $('#file-name').val(data.files);    
+    //                     $('#file-name').val(data.files);
     //                 }
     //                 else{
     //                     $('#output').html(data.files + ' selected');
     //                     $('#file-name').val(data.files);
     //                 }
-                    
+
     //             }
     //             else
     //             {
