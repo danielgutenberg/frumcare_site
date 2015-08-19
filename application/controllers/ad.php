505d3a376ce301a7ff76b0c0dd2c4da38219104d
pramod <cpramod2012@gmail.com>
2015-08-19 23:47:54 +0545

    add approval script

--- application/controllers/ad.php
+++ application/controllers/ad.php
@@ -615,8 +615,8 @@ FrumCare.com
             'from_name'   => SITE_NAME,
             'replyto'     => '',
             'replytoname' => '',
-            //'sendto'      => SITE_EMAIL,
-            'sendto'      => 'cpramod2012@gmail.com',
+            'sendto'      => SITE_EMAIL,
+            //'sendto'      => 'cpramod2012@gmail.com',
             'message'     => $msg
         );
         sendemail($param);
