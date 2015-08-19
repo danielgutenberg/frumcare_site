b90e7695492968c07ece8970d86a11f9b9bc9b38
danielguten <danielguten@gmail.com>
2015-08-19 12:20:05 +0000

    fis babysitter pagination

--- application/controllers/babysitter.php
+++ application/controllers/babysitter.php
@@ -51,6 +51,7 @@ class BabySitter extends CI_Controller{
         $locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
         $userdata       = $this->common_care_model->sort($item_per_page,$latitude,$longitude,$option,$account_category,$care_type,$distance);
         $get_total_rows = $this->common_care_model->getCount($latitude,$longitude,$account_category,$care_type,$distance);                                                         
+        $get_total_rows = count($userdata);
         $data = array(
           				'main_content' 	    => 'frontend/common_caregiver',                            
           				'title'			    => $title,
