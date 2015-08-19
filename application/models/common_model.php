f880b62062c47fe102b70aa21e65641bfbcebc60
pramod <cpramod2012@gmail.com>
2015-08-19 22:52:24 +0545

    image approval script

--- application/models/common_model.php
+++ application/models/common_model.php
@@ -291,7 +291,7 @@ class Common_Model extends CI_Model
                 return $miles;
             }
     }
-    
+
     public function getAllAdImages(){
         $sql= "SELECT * FROM tbl_user LEFT JOIN tbl_userprofile ON tbl_userprofile.user_id=tbl_user.id WHERE tbl_userprofile.photo=1 ORDER BY tbl_userprofile.id desc";
         //$sql    = "select id, name, profile_picture, profile_picture_status from tbl_user order by id desc";
