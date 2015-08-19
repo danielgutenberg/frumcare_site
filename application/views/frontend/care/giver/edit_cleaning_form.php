6abb6c9b68812c50d537bde846962e2a6c710e22
pramod <cpramod2012@gmail.com>
2015-08-19 20:32:12 +0545

    image approval

--- application/views/frontend/care/giver/edit_cleaning_form.php
+++ application/views/frontend/care/giver/edit_cleaning_form.php
@@ -30,17 +30,17 @@ $rate_type = explode(',',$detail[0]['rate_type']);
             <div class="top-welcome">
                 <h2 class="step3">Edit Job Details</h2>
             </div>
-            
+
             <div>
                 <label>Looking to work in</label>
                 <div class="form-field">
                     <div class="checkbox"><input type="checkbox" value="Private home" name="looking_to_work[]" <?php if(in_array('Private home',$templookingtowork)){?> checked="checked" <?php }?>> <span>Home</span></div>
                     <div class="checkbox"><input type="checkbox" value="Business/Office" name="looking_to_work[]" <?php if(in_array('Business/Office',$templookingtowork)){?> checked="checked" <?php }?>> <span>Office / business</span></div>
-                    <div class="checkbox"><input type="checkbox" value="Cleaning company" name="looking_to_work[]" <?php if(in_array('Cleaning company',$templookingtowork)){?> checked="checked" <?php }?>> <span>Cleaning company</span></div>                
+                    <div class="checkbox"><input type="checkbox" value="Cleaning company" name="looking_to_work[]" <?php if(in_array('Cleaning company',$templookingtowork)){?> checked="checked" <?php }?>> <span>Cleaning company</span></div>
                 </div>
             </div>
-            
-           
+
+
         <!--<div>-->
         <!--    <label>Number of rooms</label>-->
         <!--    <div class="form-field">-->
@@ -61,7 +61,7 @@ $rate_type = explode(',',$detail[0]['rate_type']);
                 </select>
                 </div>
             </div>
-            
+
             <div class="rate-select">
                 <label>Rate</label>
                 <div class="form-field">
@@ -81,13 +81,13 @@ $rate_type = explode(',',$detail[0]['rate_type']);
                 <!--<div class="checkbox"><input type="checkbox" name="rate_type[]" value="1">Hourly Rate</div>-->
                 <div class="checkbox"><input type="checkbox" name="rate_type[]" value="2">Monthly Rate Available</div>
             </div>
-            
+
             <div>
                 <label>Specialize in</label>
                 <div class="form-field">
                     <div class="checkbox"><input type="checkbox" value="Dishes" name="willing_to_work[]" <?php if(in_array('Dishes', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Dishes</span></div>
                     <div class="checkbox"><input type="checkbox" value="Floors" name="willing_to_work[]" <?php if(in_array('Floors', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Floors</span></div>
-                    <div class="checkbox"><input type="checkbox" value="Windows" name="willing_to_work[]" <?php if(in_array('Windows', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Windows</span></div>                    
+                    <div class="checkbox"><input type="checkbox" value="Windows" name="willing_to_work[]" <?php if(in_array('Windows', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Windows</span></div>
                     <div class="checkbox"><input type="checkbox" value="Laundry" name="willing_to_work[]" <?php if(in_array('Laundry', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Laundry</span></div>
                     <div class="checkbox"><input type="checkbox" value="Folding" name="willing_to_work[]" <?php if(in_array('Folding', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Folding</span></div>
                     <div class="checkbox"><input type="checkbox" value="Ironing" name="willing_to_work[]" <?php if(in_array('Ironing', $tempwillingtowork)){?> checked="checked" <?php }?>> <span>Ironing</span></div>
@@ -100,16 +100,16 @@ $rate_type = explode(',',$detail[0]['rate_type']);
             </div>
             <div>
                 <label>Availability</label>
-                <div class="form-field">                    
+                <div class="form-field">
                     <div class="checkbox"><input type="checkbox" value="Immediate" name="availability[]" <?php if(in_array("Immediate",$time)){?> checked="checked"<?php }?>>Immediate</div>
                     <div class="checkbox full"><input type="checkbox" id="ckbox1" value="Start Date" name="availability[]" <?php if(in_array("Start Date",$time)){?> checked="checked"<?php }?>> Start Date<input type="text" name="start_date" <?php if($date!='0000-00-00'){ echo 'value='.$date;}?> id="textbox1"/></div>
                     <div class="checkbox"><input type="checkbox" value="Occassionally" name="availability[]" <?php if(in_array("Occassionally",$time)){?> checked="checked"<?php }?>> <span>Occassionally</span></div>
                     <div class="checkbox"><input type="checkbox" value="Regularly" name="availability[]" <?php if(in_array("Regularly",$time)){?> checked="checked"<?php }?>> <span>Regularly</span></div>
                     <div class="checkbox"><input type="checkbox" value="Morning" name="availability[]" <?php if(in_array('Morning', $time)){?> checked="checked" <?php }?>> <span>Morning</span></div>
                     <div class="checkbox"><input type="checkbox" value="Afternoon" name="availability[]" <?php if(in_array('Afternoon', $time)){?> checked="checked" <?php }?>> <span>Afternoon</span></div>
-                    <div class="checkbox"><input type="checkbox" value="Evening" name="availability[]" <?php if(in_array('Evening', $time)){?> checked="checked" <?php }?>> <span>Evening</span></div>                                        
+                    <div class="checkbox"><input type="checkbox" value="Evening" name="availability[]" <?php if(in_array('Evening', $time)){?> checked="checked" <?php }?>> <span>Evening</span></div>
                     <div class="checkbox"><input type="checkbox" value="Weekends Fri./ Sun." name="availability[]" <?php if(in_array("Weekends Fri./ Sun.",$time)){?> checked="checked"<?php }?>> <span>Weekends Fri./ Sun.</span></div>
-                    <div class="checkbox"><input type="checkbox" value="Saturday" name="availability[]" <?php if(in_array("Saturday",$time)){?> checked="checked"<?php }?>> <span>Saturday</span></div>               
+                    <div class="checkbox"><input type="checkbox" value="Saturday" name="availability[]" <?php if(in_array("Saturday",$time)){?> checked="checked"<?php }?>> <span>Saturday</span></div>
                 </div>
             </div>
             <div>
@@ -129,11 +129,11 @@ $rate_type = explode(',',$detail[0]['rate_type']);
             <label></label>
             <input type="hidden" id="file-name" name="file">
             <button class="btn btn-primary" id="select_file">Select File</button>
-            <input type="file" name="file_upload" id="file_upload" style="display: none;"> 
+            <input type="file" name="file_upload" id="file_upload" style="display: none;">
             <div id="output" class="loader"></div>
         </div>
-        
-        
+
+
     <!--    <h2>Additional Requirements</h2>-->
     <!--    <div>-->
     <!--    <label>Languages necessary</label>-->
@@ -183,7 +183,7 @@ $rate_type = explode(',',$detail[0]['rate_type']);
                 <div>
                  <input type="submit" class="btn btn-success" value="Update"/>
              </div>
-            
+
         </div>
         </div>
     </form>
@@ -193,7 +193,7 @@ $rate_type = explode(',',$detail[0]['rate_type']);
 <script>
 
 $("#ref_check1").click(function(){
-            $(".refrence_file").show();   
+            $(".refrence_file").show();
         });
         $("#ref_check2").click(function(){
              	$.ajax({
@@ -204,8 +204,8 @@ $("#ref_check1").click(function(){
                         $('#output').html(r);
 			         }
 		          });
-                     $(".refrence_file").hide(); 
-             $('#file-name').val('');   
+                     $(".refrence_file").hide();
+             $('#file-name').val('');
         });
 //     $(document).ready(function(){
 //      if($('#ckbox1').is(':checked')){
@@ -217,10 +217,10 @@ $("#ref_check1").click(function(){
 
 //     $("#ckbox1").change(function(){
 //         if($('#ckbox1').is(':checked')){
-//             $("#textbox1").show();   
+//             $("#textbox1").show();
 //         }else{
-//             $("#textbox1").hide(); 
-//             $('#textbox1').val('');       
+//             $("#textbox1").hide();
+//             $('#textbox1').val('');
 //         }
 //     });
 // });
