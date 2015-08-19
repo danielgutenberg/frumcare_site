e56f55ccd0d55b539aef391472c720343444002b
pramod <cpramod2012@gmail.com>
2015-08-19 22:39:32 +0545

    frontend filter for image approval

--- application/models/common_care_model.php
+++ application/models/common_care_model.php
@@ -16,8 +16,8 @@ class Common_care_model extends CI_Model
         if($option == 'tbl_userprofile.id'){
             $order_type = 'desc';
         }
-        $sql = "SELECT *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance FROM tbl_user LEFT OUTER JOIN tbl_userprofile ON tbl_user.id = tbl_userprofile.user_id  left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type WHERE tbl_userprofile.profile_status = 1 AND tbl_userprofile.photo_status=1 ";
-          
+        $sql = "SELECT *,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance FROM tbl_user LEFT OUTER JOIN tbl_userprofile ON tbl_user.id = tbl_userprofile.user_id  left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type WHERE tbl_userprofile.profile_status = 1 OR tbl_userprofile.photo_status=1 ";
+
         if($care_type!=''){
             $sql.=" and tbl_userprofile.care_type = $care_type and tbl_userprofile.account_category = $account_category";
         }
