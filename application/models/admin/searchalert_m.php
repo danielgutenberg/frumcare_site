62a57bd9177082513eb0e2cb71ca12dac86343c7
pramod <cpramod2012@gmail.com>
2015-08-20 20:53:13 +0545

    search alert code

--- application/models/admin/searchalert_m.php
+++ application/models/admin/searchalert_m.php
@@ -5,3 +5,13 @@
  * Date: 8/20/15
  * Time: 7:09 PM
  */
+class Searchalert_m extends CI_Model{
+    function __construct(){
+        parent::__construct();
+        $this->_table="tbl_searchhistory";
+    }
+
+    function get_all(){
+        return $this->db->select('*')->from($this->_table)->join('tbl_user','tbl_user.id='.$this->_table.'.user_id')->join('tbl_care','tbl_care.id='.$this->_table.'.care_type')->order_by($this->_table.'.searcheddate','desc')->get()->result();
+    }
+}
