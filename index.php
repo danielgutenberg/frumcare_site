adaeab9e5a5e951568cb7ab2fb3ffde34a2514fb
pramod <cpramod2012@gmail.com>
2015-08-20 20:51:02 +0545

    index correction

--- index.php
+++ index.php
@@ -33,8 +33,9 @@ if (defined('ENVIRONMENT'))
 	switch (ENVIRONMENT)
 	{
 		case 'development':
-			error_reporting(E_ALL);
-			ini_set('display_errors', true);
+			//error_reporting(E_ALL);
+			//ini_set('display_errors', true);
+            error_reporting(0);
 		break;
 
 		case 'testing':
