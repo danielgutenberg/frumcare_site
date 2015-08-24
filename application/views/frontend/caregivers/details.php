4a1ee165999f305deceeadbb8442f41a80898947
pramod <cpramod2012@gmail.com>
2015-08-24 22:42:50 +0545

    map in details page

--- application/views/frontend/caregivers/details.php
+++ application/views/frontend/caregivers/details.php
@@ -618,17 +618,6 @@ if($recordData['care_type'] < 25 && $recordData['care_type'] > 16 ){ ?>
 </div>
 </div>
 </div>
-<?php
-$Address = urlencode($recordData['location']);
-          $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$Address."&sensor=true";
-          $xml = simplexml_load_file($request_url) or die("url not loading");
-          $status = $xml->status;
-          if ($status=="OK") {
-              $Lat = $xml->result->geometry->location->lat;
-              $Lon = $xml->result->geometry->location->lng;
-
-          }
-          ?>
 
 <link rel="stylesheet" href="<?php echo base_url();?>css/jquery.raty.css">
 <script src="<?php echo base_url();?>js/jquery.raty.js"></script>
@@ -731,8 +720,8 @@ $Address = urlencode($recordData['location']);
 
         new GMaps({
           div: '#map',
-          lat: <?php echo $lat;,
-          lng: <?php echo $lon?>
+          lat: -12.043333,
+          lng: -77.028333
         });
 
 
