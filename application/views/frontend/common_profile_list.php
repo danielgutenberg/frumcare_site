79a385eca316589f33de15ed749c6ef13189a955
pramod <cpramod2012@gmail.com>
2015-09-06 18:19:49 +0545

    location

--- application/views/frontend/common_profile_list.php
+++ application/views/frontend/common_profile_list.php
@@ -79,6 +79,14 @@
 
                     }
 
+                    $lat = $data['lat'];
+                    $lng = $data['lon'];
+                    $json = file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&sensor=false");
+                    $json_data = json_decode($json);
+                    $t= $json_data->results;
+                    print_r($t);
+                    
+
 
 
 
