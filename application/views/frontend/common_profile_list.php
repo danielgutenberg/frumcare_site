d8db62b064c075f2401bbdc634dd7637a515ff36
pramod <cpramod2012@gmail.com>
2015-09-06 18:55:49 +0545

    location

--- application/views/frontend/common_profile_list.php
+++ application/views/frontend/common_profile_list.php
@@ -75,7 +75,7 @@
         	<div class="profile-list-details col-md-9 col-sm-9 col-xs-12">
                     <?php
                     echo '<pre>';
-                    print_r($json_data->results[5]);
+                    print_r($json_data->results);
                     echo '</pre>';
                     ?>
 
