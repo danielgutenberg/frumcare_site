aa2f65fefdf3b448f73af12b9b275566d9f114f8
pramod <cpramod2012@gmail.com>
2015-09-06 21:12:48 +0545

    location

--- application/views/frontend/common_profile_list.php
+++ application/views/frontend/common_profile_list.php
@@ -64,12 +64,11 @@
                     $lng = $data['lng'];
                     $json = file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&sensor=false");
                     $json_data = json_decode($json);
-                    print_r($json_data->results[5]);
 
 
 
 
-                    $formated_add=$json_data->results[5]->formatted_address;
+                    echo $formated_add=$json_data->results[5]->formatted_address;
 
 
                     if(preg_match('/'.$location1[0].'/',$data['location'])){
