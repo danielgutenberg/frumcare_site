773caafc6426259792628b2a4f48da35573c39e7
pramod <cpramod2012@gmail.com>
2015-09-07 20:50:56 +0545

    location

--- application/controllers/caregivers.php
+++ application/controllers/caregivers.php
@@ -50,7 +50,8 @@ class Caregivers extends CI_Controller
         }
             $locationdetails = ['lat' => $latitude, 'lng' => $longitude, 'place' => $location];
             $userdata       = $this->common_care_model->sort($item_per_page,$latitude,$longitude,$option,$account_category,$care_type,$distance);
-            $get_total_rows = count($userdata);                                                         
+            print_r($userdata); exit;
+            $get_total_rows = count($userdata);
             $data = array(
               				'main_content' 	    => 'frontend/common_caregiver',                            
               				'title'			    => $title,
@@ -91,7 +92,7 @@ class Caregivers extends CI_Controller
                     $location = isset($ipdata['city'])?$ipdata['city']:'your city';
                 }             
             }
-            $userdata       = $this->common_care_model->sort($item_per_page,$latitude,$longitude,$option,$account_category,$care_type,$distance);
+            $userdata       = $this->common_care_model->sort($item_per_page,$latitude,$longitude,$option,$account_category,$care_type,$distance,$location);
             $get_total_rows = $this->common_care_model->getCount($latitude,$longitude,$account_category,$care_type,$distance);                                                         
             $data = array(
               				'main_content' 	    => 'frontend/common_caregiver',                            
