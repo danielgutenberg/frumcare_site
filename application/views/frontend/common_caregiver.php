53857b4deb237b3e5e514a5c640d13074b43c5c2
pramod <cpramod2012@gmail.com>
2015-09-09 19:52:32 +0545

    pagination issue

--- application/views/frontend/common_caregiver.php
+++ application/views/frontend/common_caregiver.php
@@ -298,7 +298,7 @@ $pagi=$pagination;
     <?php $this->load->view('frontend/common_profile_list', array('userdatas'=>$userdatas,'userlogs'=>$userlogs));
      ?>
 	</div>
-	<div class="navigations"><?php echo $pagi; ?></div>
+	<div class="navigations navi"><?php echo $pagi; ?></div>
 	</div>
 </div>
 <script type="text/javascript">
@@ -337,6 +337,7 @@ $pagi=$pagination;
 						$('#list_container').html(pagedata);
 						$('#total').text(json.total_rows);
                         $('.navigations').html(json.pagination);
+                        $('.navi').html(json.pagination);
 
                     });
                 }
