09228d7431afb80ce279f1d2b0b68705a666e336
danielguten <danielguten@gmail.com>
2015-09-16 11:48:21 +0000

    fix locations in edit personal details

--- application/views/frontend/user/account.php
+++ application/views/frontend/user/account.php
@@ -83,21 +83,24 @@
                         </div> <?php
                     } ?>
                     <div class="first-names">
-                        <label id="locationField">Location</label>
-                        <input type="hidden" id="lat" name="lat" value="<?php echo isset($lat)?$lat:''?>"/>
-                        <input type="hidden" id="lng" name="lng" value="<?php echo isset($lng)?$lng:''?>"/>
-                        <input type="hidden" id="cityName" name="city" value="<?php echo isset($city)?$city:''?>"/>
-                        <input type="hidden" id="stateName" name="state" value="<?php echo isset($state)?$state:''?>"/>
-                        <input type="hidden" id="countryName" name="country" value="<?php echo isset($country)?$country:''?>"/>
-                        <input type="text" name="location" class="required" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
+                        <label>Location</label>
+                        <div id="locationField">
+                            <input type="hidden" id="lat" name="lat" value="<?php echo isset($lat)?$lat:''?>"/>
+                            <input type="hidden" id="lng" name="lng" value="<?php echo isset($lng)?$lng:''?>"/>
+                            <input type="hidden" id="cityName" name="city" value="<?php echo isset($city)?$city:''?>"/>
+                            <input type="hidden" id="stateName" name="state" value="<?php echo isset($state)?$state:''?>"/>
+                            <input type="hidden" id="countryName" name="country" value="<?php echo isset($country)?$country:''?>"/>
+                            <input type="text" name="location" class="required" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
+                        </div>
+                        <span style="color:red;" id="error"> </span>
                     </div>
-                    <div class="small-seperator"></div>
+                    <!--<div class="small-seperator"></div>-->
                     
 
-                    <div class="first-names">
-                        <label>Neighborhood / Street</label>
-                        <input type="text" name="neighborhood" placeholder="Neighborhood" class="required" value="<?php echo (isset($neighbourhood)) ? $neighbourhood : '' ?>"/>
-                    </div>
+                    <!--<div class="first-names">-->
+                    <!--    <label>Neighborhood / Street</label>-->
+                    <!--    <input type="text" name="neighborhood" placeholder="Neighborhood" class="required" value="<?php echo (isset($neighbourhood)) ? $neighbourhood : '' ?>"/>-->
+                    <!--</div>-->
 
                     <?php /*
                     <div class="first-names">
