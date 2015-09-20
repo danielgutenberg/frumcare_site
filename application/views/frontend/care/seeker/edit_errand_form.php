1175ecd4fb922abb85c6910b20f0fd610f48dd46
danielguten <danielguten@gmail.com>
2015-09-20 17:03:49 +0000

    remove complete from location placeholder

--- application/views/frontend/care/seeker/edit_errand_form.php
+++ application/views/frontend/care/seeker/edit_errand_form.php
@@ -110,7 +110,7 @@ if($detail){
                 <input type="hidden" id="cityName" name="city" value="<?php echo isset($city)?$city:''?>"/>
                 <input type="hidden" id="stateName" name="state" value="<?php echo isset($state)?$state:''?>"/>
                 <input type="hidden" id="countryName" name="country" value="<?php echo isset($country)?$country:''?>"/>
-                <input type="text" name="location" class="required" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
+                <input type="text" name="location" class="required" placeholder="Please enter a street address" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
             </div> 
              <span style="color:red;" id="error"> </span>
         </div>
--- application/views/frontend/care/seeker/therapist_form.php
+++ application/views/frontend/care/seeker/therapist_form.php
@@ -96,7 +96,7 @@ if(($this->uri->segment(2) != 'new_profile')){?>
             <input type="hidden" id="cityName" name="city"/>
             <input type="hidden" id="stateName" name="state"/>
             <input type="hidden" id="countryName" name="country"/>
-                <input type="text" name="location" class="required" placeholder="Please enter a complete street address" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
+                <input type="text" name="location" class="required" placeholder="Please enter a street address" id="autocomplete" value="<?php echo isset($address)? $address:''; ?>" required/>
             </div>
             <span style="color:red;" id="error"> </span>
 </div>
