30bf55e8a6aaee95758fff6ea78da22827e56e09
danielguten <danielguten@gmail.com>
2015-10-26 10:57:58 +0000

    styling

--- application/views/frontend/signup/signup_form.php
+++ application/views/frontend/signup/signup_form.php
@@ -38,11 +38,11 @@ if($this->uri->segment(2)!='edit'){
 
 <?php  } ?> 
 
-<div class="container sign-up-forms">
-    <div class="signUpLeft" style="margin-left:-225px">
+<div class="container sign-up-forms" style="width=800px";>
+    <div class="signUpLeft" style="width=400px;display=inline-block">
     <?php flash();?>
     <?php if(segment(3) == '') { ?>
-    <h2 style="margin-left:-225px">
+    <h2>
         Create your account
     </h2>
     <p>
@@ -94,7 +94,7 @@ if($this->uri->segment(2)!='edit'){
         </div>
     
         <span class="sign-up-btn" style="margin-top:5px !important; margin-left:-15px;">
-            <input style="margin-left:-225px" id="submit-btn" type="submit" class="btn btn-success" value="<?php echo segment(3) != '' ? 'Save' : 'Sign up'; ?>"/>
+            <input id="submit-btn" type="submit" class="btn btn-success" value="<?php echo segment(3) != '' ? 'Save' : 'Sign up'; ?>"/>
          </span>
     </form>
     </div>
@@ -106,10 +106,10 @@ if($this->uri->segment(2)!='edit'){
     <?php } ?>
     </div>
     
-    <div class="signUpRight">
+    <div class="signUpRight" style="width=400px;display=inline-block">
         <h2>Looking for a Care Job?</h2>
         <p>Find a well paying job on FrumCare!  Hundreds of families in YOUR area are looking for trustworthy caregivers just like you. Sign up and connect with families now!</p>
-        <span style="font-family: wingdings; font-size: 200%;">&#252; Search Jobs in your area</span>
+        <span style="font-family: wingdings; font-size: 200%;">&check; Search Jobs in your area</span>
     </div>
 </div>
 
