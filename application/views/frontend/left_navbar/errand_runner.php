db753c87526d64a601df98832ecd0ca8770adc07
danielguten <danielguten@gmail.com>
2015-11-22 13:42:14 +0000

    fix left navbar search for observance

--- application/views/frontend/left_navbar/errand_runner.php
+++ application/views/frontend/left_navbar/errand_runner.php
@@ -5,47 +5,16 @@
 	  		<div class="left-search-panel col-lg-3 col-md-3 col-sm-3 col-xs-12">
 	 	<h4>Advanced Search</h4>
 	 	<form method="post" id="left-nav" action="">
- 			<div>
-	 			<label>Age of Caregiver</label>
-	 			<input type="text" name="caregiverage_from" value="" placeholder="FROM" style="width:25%" class="caregiverage_from"> to  
-	 			<input type="text" name="caregiverage_to" value="" placeholder="TO" style="width:25%" class="caregiverage_to">
-	 		</div>
-
-	 		<?php $this->load->view('frontend/left_navbar/fields/gender_of_caregiver'); ?>
-            
-            <div id="smoker">
-	 			<label>Smoker</label>
-	 			<div class="radio-half"><input type="radio" name="smoker" value="1" class="smoker"> Yes</div>
-	 			<div class="radio-half"><input type="radio" name="smoker" value="2" class="smoker"> No</div>
-	 		</div>
-            
-	 		<div>
-	 			<label>Languages</label>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="English" class="lang">English</div>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="Yiddish" class="lang">Yiddish</div>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="Hebrew" class="lang">Hebrew</div>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="Russian" class="lang">Russian</div>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="French" class="lang">French</div>
-	 			<div class="checkbox"><input type="checkbox" name="languages[]" value="Other" class="lang">Other</div>
-	 		</div>
-	 		<div>
-	 			<label>Level of observance (check one or more)</label>
-	 			<div class="checkbox"><input type="checkbox" value="Yeshivish/ Chasidish" name="observance[]" class="observance">Yeshivish / Chasidish</div>
-	 			<div class="checkbox"><input type="checkbox" value="Orthodox/ Modern Orthodox" name="observance[]" class="observance">Orthodox / Modern orthodox</div>	 			
-	 			<div class="checkbox"><input type="checkbox" value="Familiar With Jewish Tradition" name="observance[]" class="observance">Familiar with Jewish Tradition</div>
-	 			<div class="checkbox"><input type="checkbox" value="Any" name="observance[]" class=" observance">Any</div>	 			
-	 		</div>
-	 		<div class="year-exp">
-		 		<span>Minimum Experience</span>
-		 			<select name="year_experience" class="required year_experience">
-		 			<option value="">--select--</option>
-		 				<option value="1">1 year</option>	
-		 				<option value="2">2 years</option>	
-		 				<option value="3">3 years</option>	
-		 				<option value="4">4 years</option>	
-		 				<option value="6">5+ years</option>	
-		 			</select>
-		 	</div>
+	 		<?php
+	 		
+	 		$this->load->view('frontend/left_navbar/fields/age_of_caregiver');
+	 		$this->load->view('frontend/left_navbar/fields/gender_of_caregiver'); 
+	 		$this->load->view('frontend/left_navbar/fields/smoker');
+	 		$this->load->view('frontend/left_navbar/fields/languages');
+	 		$this->load->view('frontend/left_navbar/fields/observance_of_caregiver');
+	 		$this->load->view('frontend/left_navbar/fields/minimum_experience');
+	 			 		
+	 		?>
 
 		 	<div>
 		 		<label>Availability</label>
