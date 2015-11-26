ec6a1339078a604ff67da0e359f310c78f2b40fc
danielguten <danielguten@gmail.com>
2015-11-26 10:13:04 +0000

    update css for email

--- application/views/frontend/email/profile_list.php
+++ application/views/frontend/email/profile_list.php
@@ -41,26 +41,26 @@ $careType = [
                 }
 // 			$reviewData = Review_model::countReviewById($data['id']);
             $navigate = $data['care_type']>16?'jobs':'caregivers'; ?> 	
-            <div style="margin-right: -15px; margin-left:-15px">
-            <div class="profile-img-wrap col-md-3 col-sm-3 col-xs-12" style="width:25%; float:left"> <?php
+            <div style="margin-right: -15px; margin-left:-15px; border-bottom: 1px solid #cccccc; padding-bottom: 20px; padding-top: 20px; padding-left: 10px;">
+            <div class="profile-img-wrap col-md-3 col-sm-3 col-xs-12" style="width:15%; float:left"> <?php
                 if($data['profile_picture']!="" && file_exists('images/profile-picture/'.$data['profile_picture'])) {?>
                     <a href="<?php echo site_url().$navigate;?>/details/<?php echo $data['uri'];?>/<?php echo $data['care_type'];?>">
     		            <div id="profile_image">
-    		            	<img src="<?php echo site_url("images/profile-picture/{$data['profile_picture']}")?>">
+    		            	<img style="height:100px; width:100px" src="<?php echo site_url("images/profile-picture/{$data['profile_picture']}")?>">
     		            </div>
     	            </a><?php }
                 else {  
                     if($data['photo_of_child']!="" && file_exists('images/profile-picture/'.$data['photo_of_child'])) {?>
                         <a href="<?php echo site_url().$navigate;?>/details/<?php echo $data['uri'];?>/<?php echo $data['care_type'];?>">
         		            <div id="profile_image">
-        		            	<img src="<?php echo site_url("images/profile-picture/{$data['photo_of_child']}")?>">
+        		            	<img style="height:100px; width:100px" src="<?php echo site_url("images/profile-picture/{$data['photo_of_child']}")?>">
         		            </div>
         	            </a><?php } else { ?>
                 
                 
                     <a href="<?php echo site_url().$navigate;?>/details/<?php echo $data['uri'];?>/<?php echo $data['care_type'];?>">
 			            <div id="profile_image">
-			            	<img src="<?php echo site_url("images/no-image.jpg")?>">
+			            	<img style="height:100px; width:100px" src="<?php echo site_url("images/no-image.jpg")?>">
                         </div>
                     </a><?php 
                 }} ?>
@@ -91,7 +91,7 @@ $careType = [
     	        	?>
     	        </div>
 	        </div>
-        	<div style="float:left; width:75%;">
+        	<div style="float:left; width:85%;">
                 <?php if ($data['account_category'] == 3) {?>
                 <span style="color: #525252;display: inline-block;font-family: 'Lato',sans-serif;font-size: 29px;font-weight: 400;margin-bottom: 4px;margin-left: 0;margin-right: 8px;">
 					<a style="color: #525252;display: inline-block;font-family: 'Lato',sans-serif;font-size: 29px;font-weight: 400;margin-bottom: 4px;margin-left: 0;margin-right: 8px;" href="<?php echo site_url();?>jobs/details/<?php echo $data['uri'];?>/<?php echo $data['care_type'];?>"><?php echo ucwords($data['organization_name']);?></a>
@@ -293,7 +293,7 @@ $careType = [
 				 	<a style="background: none repeat scroll 0 0 #85bd30;border: medium none;font-size: 16px;margin-left: -8px;margin-top: 30px;width: 162px;color: #fff;" href="<?php echo site_url().$navigate; ?>/details/<?php echo $data['uri'];?>/<?php echo $data['care_type'];?>" class="btn btn-primary" >See full Profile</a>
 			</div>
 		</div>
-		<div class="clearfix"></div>
+		<div style="line-height: 1.42857143;color: #6a6a6a; font-family: 'Varela Round', 'Helvetica Neue', Helvetica, Arial, sans-serif;display:block;font-size: 14px;"></div>
         <?php 		
 		} //end of foreach
 	} //end of if
