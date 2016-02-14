f1146f439fc2e6a07c00112d5d7600cae88005bb
danielguten <danielguten@gmail.com>
2016-02-14 20:56:41 +0000

    fix share this profile from details page

--- application/views/frontend/caregivers/details.php
+++ application/views/frontend/caregivers/details.php
@@ -835,9 +835,9 @@ if($recordData['care_type'] < 25 && $recordData['care_type'] > 16 ){ ?>
 </script>
 
 <!-- share this starts-->
-<!--<script type="text/javascript">var switchTo5x=true;</script>-->
+<script type="text/javascript">var switchTo5x=true;</script>
 <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-<!--<script type="text/javascript">stLight.options({publisher: "fcfd27c1-d440-47b3-bb5e-17abb292ed1f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>-->
+<script type="text/javascript">stLight.options({publisher: "fcfd27c1-d440-47b3-bb5e-17abb292ed1f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 <!-- share this ends -->
 <!-- scroll js starts -->
 <script src="<?php echo site_url();?>js/jquery.localScroll.min.js"></script>
