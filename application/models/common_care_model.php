59a148e9dd9ea9470cb3c12d501d297a83da0845
danielguten <danielguten@gmail.com>
2016-03-15 08:04:05 +0000

    make sure order by is set before adding to query

--- application/models/common_care_model.php
+++ application/models/common_care_model.php
@@ -352,7 +352,7 @@ class Common_care_model extends CI_Model
             $skills               = $search['skills'];
 		}
 		
-		$sql = "select count(*) from tbl_user left outer join  tbl_userprofile on tbl_user.id = tbl_userprofile.user_id left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type where tbl_user.status = 1 and tbl_userprofile.profile_status=1";			
+		$sql = "select tbl_user.*,(((acos(sin(($latitude * pi() /180 )) * sin((`lat` * pi( ) /180 ) ) + cos( ( $latitude * pi( ) /180 ) ) * cos( (`lat` * pi( ) /180 )) * cos( (( $longitude - `lng` ) * pi( ) /180 )))) *180 / pi( )) *60 * 1.1515) AS distance, tbl_userprofile.* from tbl_user left outer join  tbl_userprofile on tbl_user.id = tbl_userprofile.user_id left outer join tbl_care on tbl_care.id = tbl_userprofile.care_type where tbl_user.status = 1 and tbl_userprofile.profile_status=1";			
 
 		if ($care_type == 'caregivers') {
 		    $sql .= " and tbl_care.service_type = 1";
@@ -542,7 +542,15 @@ class Common_care_model extends CI_Model
 		}
         if($start_date)
 		$sql .= " and tbl_userprofile.start_date='".$start_date."'";
-
+        if($distance != "unlimited" && ((int) $distance) > 0){
+            $sql.=" having distance <= $distance";
+        }
+        if($sort_by == 'tbl_userprofile.id') {
+        	$sql.= " order by tbl_userprofile.id desc";
+        }
+        if($sort_by == 'distance') {
+        	$sql.= " order by distance asc";
+        }
 		$query 	= $this->db->query($sql);
 		$res 	= $query->result_array();
 		if($res)
