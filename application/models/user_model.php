29db70eca5988d0b7a8be0afff0fe85a9b6e12b7
danielguten <danielguten@gmail.com>
2016-05-15 07:25:41 +0000

    add organiozation name

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -371,7 +371,7 @@ class User_model extends CI_Model
         $this->db->join('tbl_care','tbl_userprofile.care_type = tbl_care.id','left');
         $this->db->where('user_id',$this->session->userdata('current_user'));
         $this->db->order_by("tbl_userprofile.id", "desc");
-        $this->db->select('tbl_userprofile.*, tbl_care.*, tbl_user.city, tbl_user.state, tbl_user.country, tbl_user.name, tbl_user.uri, tbl_user.profile_picture, tbl_user.profile_picture_status');
+        $this->db->select('tbl_userprofile.*, tbl_care.*, tbl_user.city, tbl_user.state, tbl_user.country, tbl_user.name, tbl_user.organization_name, tbl_user.uri, tbl_user.profile_picture, tbl_user.profile_picture_status');
         $query = $this->db->get('tbl_userprofile');
         return $query->result();
     }
