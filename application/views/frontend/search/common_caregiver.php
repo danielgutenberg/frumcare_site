e0f2936a56733f5bc3976b5d88a46928f821f157
danielguten <danielguten@gmail.com>
2017-06-06 08:48:02 +0000

    move ads placement

--- application/views/frontend/search/common_caregiver.php
+++ application/views/frontend/search/common_caregiver.php
@@ -250,6 +250,8 @@
 	</div>
 
 </div> 
+			<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b995b62a-2c1a-4adc-94b4-639eb2be9af6"></script>
+
 <script>
     function filterCaregivers() {
         $(".searchloader").fadeIn("fast");
