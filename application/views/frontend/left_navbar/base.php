6a99fc648c7a0a2d8703d0b3eb3b9fd848538a5d
danielguten <danielguten@gmail.com>
2017-06-14 08:43:57 +0000

    remove grey on the left side of page

--- application/views/frontend/left_navbar/base.php
+++ application/views/frontend/left_navbar/base.php
@@ -9,9 +9,9 @@
 	
 </div>
 <div style="    background: none repeat scroll 0 0 #f0f0ec;
-    border-bottom: 1px solid #f0f0ec;
+    border-bottom: 1px solid white;
     border-radius: 5px;
-    border-right: 1px solid #f0f0ec;
+    border-right: 1px solid white;
     height:3500px;" 
     class="row">
 <script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b995b62a-2c1a-4adc-94b4-639eb2be9af6"></script>
