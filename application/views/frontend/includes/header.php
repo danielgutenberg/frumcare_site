1ee3e5003c8d770f0f077c012e0c84350aad26a2
danielguten <danielguten@gmail.com>
2017-09-24 12:30:49 +0000

    add links to top

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -94,13 +94,18 @@
 
 <div style="background-color: #e5e5e5;">
     <div class="headerContainer" style="margin-right: auto; margin-left: auto; height:77px; ">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="padding-top:13px">
+        <div class="col-xs-2" style="padding-top:13px">
             <a href="<?php echo base_url() ?>" style="background: url(<?php echo base_url('img/logo.png') ?>);background-repeat:no-repeat; height: 60px;width: 249px;display: inline-block;background-size: 90%;"></a>
         </div>
 
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <div class="col-xs-10">
 
             <div class="pull-right login-block" style="margin-top:25px">
+                <a style="color: #525252; font-size: 15px; padding-top: 5px" href="<?php echo base_url('caregivers') ?>">Caregivers&nbsp&nbsp|&nbsp&nbsp</a>
+                <a style="color: #525252; font-size: 15px" href="<?php echo base_url('jobs') ?>">Jobs&nbsp&nbsp|&nbsp&nbsp</a>
+                <a style="color: #525252; font-size: 15px" href="<?php echo base_url('caregivers/organizations') ?>">Institutions&nbsp&nbsp|&nbsp&nbsp</a>
+                <a style="color: #525252; font-size: 15px" href="<?php echo base_url('caregivers/therapists') ?>">Therapists&nbsp&nbsp|&nbsp&nbsp</a>
+                <a style="color: #525252; font-size: 15px; padding-right:10px" href="//www.frumcare.com/store">Shop</a>
             <?php if (!$this->session->userdata('current_user') && !$this->session->userdata('fb_id') && !$this->session->userdata('twitter_id')) { ?>
                 <a id="btn-nav-login" style="color: #525252; font-size: 16px" class="btn" href="<?php echo base_url('login') ?>">Log in</a>
                 <a id="btn-nav-signup" style="background: transparent; font-size: 16px; color:#357ebd;" class="btn btn-primary" href="<?php echo base_url('signup') ?>">SIGN UP</a>
