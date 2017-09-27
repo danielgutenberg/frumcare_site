899c0c494a6a6eb3f0820cffae83c1d82ea46987
danielguten <danielguten@gmail.com>
2017-09-27 15:08:30 +0000

    try pushing to dev

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -11,6 +11,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta title="Frumcare">
         <meta property="og:title" content="Write a Review" />
     <meta property="og:type" content="Sharing Widgets" />
     <meta property="og:url" content="<?php echo current_url(); ?>" />
